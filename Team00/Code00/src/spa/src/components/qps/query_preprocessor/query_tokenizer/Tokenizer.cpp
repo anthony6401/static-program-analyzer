1bde4b6949c42767e7daf9e7fac42cda5ff2f664
Andrea Tan <andreaxtan611@gmail.com>
2022-08-26 13:35:44 +0800

    Initialise string to token map

--- Team00/Code00/src/spa/src/components/qps/query_preprocessor/query_tokenizer/Tokenizer.cpp
+++ Team00/Code00/src/spa/src/components/qps/query_preprocessor/query_tokenizer/Tokenizer.cpp
@@ -8,6 +8,35 @@ using namespace qps;
 
 
 Tokenizer::Tokenizer(std::string query) : query(query) {
-
+    /**
+     * Initialises unordered map of string values to tokens with initializer list.
+     */
+    stringToTokenMap = {
+            {"Select", TokenType::SELECT},
+            {"such", TokenType::SUCH},
+            {"that", TokenType::THAT},
+            {"Modifies", TokenType::MODIFIES},
+            {"Uses", TokenType::USES},
+            {"_", TokenType::UNDERSCORE},
+            {",", TokenType::COMMA},
+            {"(", TokenType::OPEN_BRACKET},
+            {")", TokenType::CLOSED_BRACKET},
+            {"\"", TokenType::QUOTATION_MARK},
+            {";", TokenType::SEMI_COLON},
+            {"+", TokenType::PLUS},
+            {"-", TokenType::MINUS},
+            {"/", TokenType::DIVIDE},
+            {"*", TokenType::MULTIPLY},
+            {"%", TokenType::PERCENTAGE},
+            {"stmt", TokenType::STMT},
+            {"read", TokenType::READ},
+            {"print", TokenType::PRINT},
+            {"call", TokenType::CALL},
+            {"while", TokenType::WHILE},
+            {"if", TokenType::IF},
+            {"assign", TokenType::ASSIGN},
+            {"variable", TokenType::VARIABLE},
+            {"constant", TokenType::CONSTANT},
+            {"procedure", TokenType::PROCEDURE}
+    };
 }
-
