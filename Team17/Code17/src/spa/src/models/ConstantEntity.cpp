22eac5b81b6bdd537545b2f5f53993ef823aca07
Kacalamaka <heinrich2001.ang@gmail.com>
2022-08-30 23:56:47 +0800

    Created ConstantEntity class

---
+++ Team17/Code17/src/spa/src/models/ConstantEntity.cpp
@@ -0,0 +1,6 @@
+#include "ConstantEntity.h"
+#include "Entity.h"
+
+#include <string>
+
+ConstantEntity::ConstantEntity(std::string n) : Entity(n) {};
---
+++ Team17/Code17/src/spa/src/models/ConstantEntity.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Entity.h"
+
+class ConstantEntity : public Entity {
+	public:
+		ConstantEntity(std::string n);
+};
