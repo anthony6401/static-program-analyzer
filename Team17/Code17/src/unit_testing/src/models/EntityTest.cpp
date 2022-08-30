0de9c2840b4d64dd6e66a88a1253ec0a09e7c0cc
Kacalamaka <heinrich2001.ang@gmail.com>
2022-08-31 02:16:46 +0800

    Created unit test for Entity class

--- Team17/Code17/src/unit_testing/src/models/EntityTest.cpp
+++ Team17/Code17/src/unit_testing/src/models/EntityTest.cpp
@@ -1,17 +1,19 @@
-#include "AssignEntity.h"
-#include "CallEntity.h"
-#include "ConstantEntity.h"
-#include "Entity.h"
-#include "IfEntity.h"
-#include "PrintEntity.h"
-#include "ProcedureEntity.h"
-#include "ReadEntity.h"
-#include "StatementEntity.h"
-#include "StatementListEntity.h"
-#include "VariableEntity.h"
-#include "WhileEntity.h"
+#include "models/Entity/AssignEntity.h"
+#include "models/Entity/CallEntity.h"
+#include "models/Entity/ConstantEntity.h"
+#include "models/Entity/Entity.h"
+#include "models/Entity/IfEntity.h"
+#include "models/Entity/PrintEntity.h"
+#include "models/Entity/ProcedureEntity.h"
+#include "models/Entity/ReadEntity.h"
+#include "models/Entity/StatementEntity.h"
+#include "models/Entity/StatementListEntity.h"
+#include "models/Entity/VariableEntity.h"
+#include "models/Entity/WhileEntity.h"
 
 #include <catch.hpp>
+
+#include <vector>
 
 std::string VALUE = "1";
 
@@ -24,10 +26,91 @@ TEST_CASE("Entity Test") {
 }
 
 //Child of Entity
-TEST_CASE("Procedure Test") {
+TEST_CASE("Procedure Entity Test") {
 	ProcedureEntity p = ProcedureEntity(VALUE);
 
 	REQUIRE(p.getValue() == "1");
 
 }
+
+TEST_CASE("Assign Entity Test") {
+	AssignEntity a = AssignEntity(VALUE);
+
+	REQUIRE(a.getValue() == "1");
+
+}
+
+TEST_CASE("Call Entity Test") {
+	CallEntity c = CallEntity(VALUE);
+
+	REQUIRE(c.getValue() == "1");
+
+}
+
+TEST_CASE("Constant Entity Test") {
+	ConstantEntity ct = ConstantEntity(VALUE);
+
+	REQUIRE(ct.getValue() == "1");
+
+}
+
+TEST_CASE("Variable Entity Test") {
+	VariableEntity v = VariableEntity(VALUE);
+
+	REQUIRE(v.getValue() == "1");
+
+}
+
+//Statement Entity
+TEST_CASE("Statement Entity Test") {
+	StatementEntity st = StatementEntity(VALUE);
+
+	REQUIRE(st.getValue() == "1");
+
+}
+
+// Child of Statement Entity
+TEST_CASE("If Entity Test") {
+	IfEntity i = IfEntity(VALUE);
+
+	REQUIRE(i.getValue() == "1");
+
+}
+
+TEST_CASE("Print Entity Test") {
+	PrintEntity pr = PrintEntity(VALUE);
+
+	REQUIRE(pr.getValue() == "1");
+
+}
+
+TEST_CASE("Read Entity Test") {
+	ReadEntity rd = ReadEntity(VALUE);
+
+	REQUIRE(rd.getValue() == "1");
+
+}
+
+TEST_CASE("While Entity Test") {
+	WhileEntity w = WhileEntity(VALUE);
+
+	REQUIRE(w.getValue() == "1");
+
+}
+
+//TODO:NEED TO TEST STATEMENTLISTENTITY
+//StatementList
+//TEST_CASE("While Entity Test") {
+//	StatementEntity st1 = StatementEntity("1");
+//	StatementEntity st2 = StatementEntity("2");
+//
+//	std::vector<StatementEntity> statements{ st1, st2 };
+//
+//	StatementListEntity stmtLst = StatementListEntity(statements);
+//
+//	std::vector<StatementEntity> result = stmtLst.getStatements();
+//
+//	REQUIRE(std::equal(result.begin(), result.end(), statements.begin()));
+//
+//}
 
