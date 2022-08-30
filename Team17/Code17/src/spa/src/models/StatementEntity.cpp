2a7ab316f9b8bf747d6b2a5e1e1b4b14d9b2dee0
Kacalamaka <heinrich2001.ang@gmail.com>
2022-08-30 23:46:56 +0800

    Created StatementEntity class

---
+++ Team17/Code17/src/spa/src/models/StatementEntity.cpp
@@ -0,0 +1,6 @@
+#include "StatementEntity.h"
+#include "Entity.h"
+
+#include <string>
+
+StatementEntity::StatementEntity(std::string n) : Entity(n) {};
---
+++ Team17/Code17/src/spa/src/models/StatementEntity.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Entity.h"
+
+class StatementEntity : public Entity {
+	public:
+		StatementEntity(std::string n);
+};
