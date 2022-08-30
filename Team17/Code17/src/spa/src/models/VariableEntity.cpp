7a9f93b9e537281b4aa3cee2fda406f6d9d4b8fc
Kacalamaka <heinrich2001.ang@gmail.com>
2022-08-30 23:57:08 +0800

    Created VariableEntity class

---
+++ Team17/Code17/src/spa/src/models/VariableEntity.cpp
@@ -0,0 +1,6 @@
+#include "VariableEntity.h"
+#include "Entity.h"
+
+#include <string>
+
+VariableEntity::VariableEntity(std::string n) : Entity(n) {};
---
+++ Team17/Code17/src/spa/src/models/VariableEntity.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Entity.h"
+
+class VariableEntity : public Entity {
+	public:
+		VariableEntity(std::string n);
+};
