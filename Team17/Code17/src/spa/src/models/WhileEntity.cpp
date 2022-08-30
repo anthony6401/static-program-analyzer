e95f50e8fec6033f0a21ef95d998aea13f2f56f9
Kacalamaka <heinrich2001.ang@gmail.com>
2022-08-30 18:06:42 +0800

    Created WhileEntity class

---
+++ Team17/Code17/src/spa/src/models/WhileEntity.cpp
@@ -0,0 +1,6 @@
+#include "WhileEntity.h"
+#include "Entity.h"
+
+#include <string>
+
+WhileEntity::WhileEntity(std::string n) : Entity(n) {};
---
+++ Team17/Code17/src/spa/src/models/WhileEntity.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <Entity.h>
+
+class WhileEntity : public Entity {
+	WhileEntity(std::string n);
+};
