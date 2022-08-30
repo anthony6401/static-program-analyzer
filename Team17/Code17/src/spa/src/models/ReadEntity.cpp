85e17b569d8c34c826af21627569074fd73ce21e
Kacalamaka <heinrich2001.ang@gmail.com>
2022-08-30 23:46:40 +0800

    Created ReadEntity class

---
+++ Team17/Code17/src/spa/src/models/ReadEntity.cpp
@@ -0,0 +1,6 @@
+#include "ReadEntity.h"
+#include "Entity.h"
+
+#include <string>
+
+ReadEntity::ReadEntity(std::string n) : StatementEntity(n) {};
---
+++ Team17/Code17/src/spa/src/models/ReadEntity.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "StatementEntity.h"
+
+#include <string>
+
+class ReadEntity : public StatementEntity {
+	public:
+		ReadEntity(std::string n);
+	private:
+};
