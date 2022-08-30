51ac0133695b0c936eefefccbfc59699690fcb74
Kacalamaka <heinrich2001.ang@gmail.com>
2022-08-30 23:52:33 +0800

    Created IfEntity class

---
+++ Team17/Code17/src/spa/src/models/IfEntity.cpp
@@ -0,0 +1,6 @@
+#include "IfEntity.h"
+#include "Entity.h"
+
+#include <string>
+
+IfEntity::IfEntity(std::string n) : StatementEntity(n) {};
---
+++ Team17/Code17/src/spa/src/models/IfEntity.h
@@ -0,0 +1,12 @@
+#pragma once
+#pragma once
+
+#include "StatementEntity.h"
+
+#include <string>
+
+class IfEntity : public StatementEntity {
+	public:
+		IfEntity(std::string n);
+	private:
+};
