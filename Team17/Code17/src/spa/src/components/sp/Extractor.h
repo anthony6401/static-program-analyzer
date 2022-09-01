8c8057df9f70ac2aaa0864d0e1d71a93a80ef9f6
hyngkng <hyngkng@gmail.com>
2022-09-01 20:21:54 +0800

    Edit header file

--- Team17/Code17/src/spa/src/components/sp/Extractor.h
+++ Team17/Code17/src/spa/src/components/sp/Extractor.h
@@ -11,4 +11,18 @@ class Extractor {
 public:
 	Extractor::Extractor();
 	vector<Relationship*> extractRelationships(SimpleToken parentToken);
+	void handleProcedure(vector<Relationship*> relationships);
+	void handleStatement(vector<Relationship*> relationships);
+	void handleRead(vector<Relationship*> relationships);
+	void handlePrint(vector<Relationship*> relationships);
+	void handleCall(vector<Relationship*> relationships);
+	void handleWhile(vector<Relationship*> relationships);
+	void handleIf(vector<Relationship*> relationships);
+	void handleAssign(vector<Relationship*> relationships);
+	void handleCondExpr(vector<Relationship*> relationships);
+	void handleVariable(vector<Relationship*> relationships);
+	void handleConstant(vector<Relationship*> relationships);
+	void handleClose(vector<Relationship*> relationships);
+	void handleOpr(vector<Relationship*> relationships);
+	void handleError();
 };
