aa818ec4fd5b73ae5f94ea3ca0becc1533517397
Andrea Tan <andreaxtan611@gmail.com>
2022-09-02 09:46:35 +0800

    Add sample relationship class methods

--- Team17/Code17/src/spa/src/components/qps/abstract_query_object/Relationship.cpp
+++ Team17/Code17/src/spa/src/components/qps/abstract_query_object/Relationship.cpp
@@ -1,5 +1,15 @@
-//
-// Created by ANDREA TAN on 2/9/22.
-//
-
 #include "Relationship.h"
+
+// to be amended
+TokenType Relationship::getRelationshipType() {
+    return TokenType::INTEGER;
+}
+
+TokenObject Relationship::getLeft() {
+    return TokenObject(TokenType::INTEGER, "");
+}
+
+TokenObject Relationship::getRight() {
+    return TokenObject(TokenType::INTEGER, "");
+}
+
--- Team17/Code17/src/spa/src/components/qps/abstract_query_object/Relationship.h
+++ Team17/Code17/src/spa/src/components/qps/abstract_query_object/Relationship.h
@@ -1,8 +1,19 @@
 #ifndef SPA_RELATIONSHIP_H
 #define SPA_RELATIONSHIP_H
 
-class Relationship {
+#include "components/qps/query_preprocessor/query_tokenizer/TokenType.h"
+#include "components/qps/query_preprocessor/query_tokenizer/TokenObject.h"
 
+class Relationship {
+private:
+    TokenType relationship;
+    TokenObject left;
+    TokenObject right;
+
+public:
+    static TokenType getRelationshipType();
+    static TokenObject getLeft();
+    static TokenObject getRight();
 };
 
 #endif //SPA_RELATIONSHIP_H
