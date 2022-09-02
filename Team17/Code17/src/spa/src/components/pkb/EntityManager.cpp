d035bef3e5a27f39109d7253f8bbc6cc37391b74
Kacalamaka <heinrich2001.ang@gmail.com>
2022-09-02 23:50:43 +0800

    Created EntityManager class

---
+++ Team17/Code17/src/spa/src/components/pkb/EntityManager.cpp
@@ -0,0 +1,57 @@
+#include "EntityManager.h"
+
+#include "storage/AssignEntityStorage.h"
+#include "storage/CallEntityStorage.h"
+#include "storage/ConstantEntityStorage.h"
+#include "storage/EntityStorage.h"
+#include "storage/IfEntityStorage.h"
+#include "storage/PrintEntityStorage.h"
+#include "storage/ProcedureEntityStorage.h"
+#include "storage/ReadEntityStorage.h"
+#include "storage/StatementEntityStorage.h"
+#include "storage/VariableEntityStorage.h"
+#include "storage/WhileEntityStorage.h"
+
+EntityManager::EntityManager() {
+	AssignEntityStorage assStore = new AssignEntityStorage();
+	CallEntityStorage callStore = new CallEntityStorage();
+	ConstantEntityStorage constStore = new ConstantEntityStorage();
+	IfEntityStorage ifStore = new IfEntityStorage();
+	PrintEntityStorage prStore = new PrintEntityStorage();
+	ProcedureEntityStorage procStore = new ProcedureEntityStorage();
+	ReadEntityStorage reStore = new ReadEntityStorage();
+	StatementEntityStorage stStore = new StatementEntityStorage();
+	VariableEntityStorage varStore = new VariableEntityStorage();
+	WhileEntityStorage whileStore = new WhileEntityStorage();
+
+	//Add each entity into the array
+	entityStore.push_back(assStore);
+	entityStore.push_back(callStore);
+	entityStore.push_back(constStore);
+	entityStore.push_back(ifStore);
+	entityStore.push_back(prStore);
+	entityStore.push_back(procStore);
+	entityStore.push_back(reStore);
+	entityStore.push_back(stStore);
+	entityStore.push_back(varStore);
+	entityStore.push_back(whileStore);
+}
+
+//Returns true, if it manage to stored, else false
+bool EntityManager::storeEntity(Entity entity) {
+
+	bool ret = false;
+	for (auto& st : entityStore) {
+		ret = ret || st->storeEntity(Entity);
+	}
+	return ret;
+}
+
+std::vector<std::string> EntityManager::getEntity(Entity entity) {
+	//TODO: to be implemented
+	return 0;
+}
+
+
+
+
---
+++ Team17/Code17/src/spa/src/components/pkb/EntityManager.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "storage/EntityStorage.h"
+
+#include <string>
+#include <vector>
+
+class EntityManager {
+public:
+	EntityManager();
+	std::vector<std::string> getEntity(Entity entity);
+	bool storeEntity(Entity entity);
+
+private:
+	 std::vector<EntityStorage*> entityStore;
+};
