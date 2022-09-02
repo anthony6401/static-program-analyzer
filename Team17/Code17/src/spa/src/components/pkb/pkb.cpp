685282e2e55d4f77b66f9078d914c161952b23a0
Kacalamaka <heinrich2001.ang@gmail.com>
2022-09-02 19:24:11 +0800

    added empty definition for pkb constructor (quick fix)

--- Team17/Code17/src/spa/src/components/pkb/pkb.cpp
+++ Team17/Code17/src/spa/src/components/pkb/pkb.cpp
@@ -3,8 +3,9 @@
 #include "../../models/Relationship/Relationship.h"
 #include "pkb.h"
 
-PKB::PKB();
+PKB::PKB() {};
 
 bool PKB::storeRelationship(Relationship* rel) {
 	//need to update to check if statement with
+	return 0;
 }
