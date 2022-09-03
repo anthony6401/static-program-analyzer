3d1261edc8855cf6c381fde155143f6fff65ad36
Kacalamaka <heinrich2001.ang@gmail.com>
2022-09-03 19:51:11 +0800

    Added API from QPSClient

--- Team17/Code17/src/spa/src/components/pkb/pkb.cpp
+++ Team17/Code17/src/spa/src/components/pkb/pkb.cpp
@@ -1,8 +1,18 @@
 #include "../../models/Relationship/Relationship.h"
+
 #include "./manager/EntityManager.h"
+
 #include "pkb.h"
 
-PKB::PKB() {};
+#include "../qps/query_preprocessor/query_tokenizer/TokenType.h"
+
+PKB::PKB() {
+	entityManager = new EntityManager();
+};
+
+std::vector<std::string> PKB::getAllEntity(qps::TokenType returnType) {
+	return entityManager->getAllEntity(returnType);
+}
 
 bool PKB::storeRelationship(Relationship* rel) {
 	//to be implemented
--- Team17/Code17/src/spa/src/components/pkb/pkb.h
+++ Team17/Code17/src/spa/src/components/pkb/pkb.h
@@ -4,9 +4,15 @@
 
 #include "./manager/EntityManager.h"
 
+#include "../qps/query_preprocessor/query_tokenizer/TokenType.h"
+
 class PKB {
 public:
 	PKB();
+	//For getting entity
+	std::vector<std::string> getAllEntity(qps::TokenType returnType);
+
+	//For getting relationship
 	bool storeRelationship(Relationship* rel);
 private:
 	//Add managers
