8668cbc4bd99b2d87be72d7f51332e5b75d39b36
Andrea Tan <andreaxtan611@gmail.com>
2022-09-04 00:42:56 +0800

    Add evaluator test file

---
+++ Team17/Code17/src/unit_testing/src/qps/query_evaluator/EvaluatorTest.cpp
@@ -0,0 +1,16 @@
+#include <catch.hpp>
+#include <list>
+#include "components/qps/abstract_query_object/QueryObject.h"
+
+//QueryObject(std::vector<SuchThat> relationships,
+//Declaration declarations, std::vector<Pattern> patterns, Select select,
+//        std::unordered_map<std::string, TokenType> synonymToDesignEntityMap);
+
+//TEST_CASE("Evaluation for Select Clause - variable v; Select v") {
+//    std::vector<SuchThat> relationships {};
+//    std::vector<Pattern> patterns {};
+//    Select select = Select(TokenType::VARIABLE, "v");
+//    QueryObject testQuery = QueryObject()
+//
+//    REQUIRE(testResult == expectedResult);
+//}
