ac8f22caa0306962c39eeaffde32cd77bcf098d5
Anthony Lie <e0407672@u.nus.edu>
2022-09-03 17:16:11 +0800

    Move getSize implementation from header to cpp file

--- Team17/Code17/src/spa/src/components/pkb/storage/EntityStorage.h
+++ Team17/Code17/src/spa/src/components/pkb/storage/EntityStorage.h
@@ -9,10 +9,7 @@ public:
 	EntityStorage();
 	virtual bool storeEntity(Entity* entity) = 0;
 	virtual std::unordered_set<Entity*>* getAllEntity(qps::TokenType tokenType) = 0;
-
-	int getSize() {
-		return  set->size();
-	}
+	int getSize();
 protected:
 	std::unordered_set<Entity*>* set;
 };
