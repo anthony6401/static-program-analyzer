52c097327aaddfd76906a41a8ea32a0ab09107e6
Kacalamaka <heinrich2001.ang@gmail.com>
2022-09-03 19:47:34 +0800

    Created QPSClient API

--- Team17/Code17/src/spa/src/components/pkb/clients/QPSClient.cpp
+++ Team17/Code17/src/spa/src/components/pkb/clients/QPSClient.cpp
@@ -1,2 +1,17 @@
 #include "QPSClient.h"
 
+#include "../../qps/query_preprocessor/query_tokenizer/TokenType.h"
+
+#include "../pkb.h"
+
+#include <string>
+#include <vector>
+
+QPSClient::QPSClient(PKB* knowledge) {
+	pkb = knowledge;
+}
+
+std::vector<std::string> QPSClient::getAllEntity(qps::TokenType returnType) {
+	return pkb->getAllEntity(returnType);
+}
+
--- Team17/Code17/src/spa/src/components/pkb/clients/QPSClient.h
+++ Team17/Code17/src/spa/src/components/pkb/clients/QPSClient.h
@@ -1,5 +1,26 @@
+#pragma once
+#include "../../qps/query_preprocessor/query_tokenizer/TokenType.h"
 
+#include "../pkb.h"
+
+#include <string>
+#include  <vector>
 
 class QPSClient {
 
+public:
+
+	QPSClient(PKB *knowledge);
+
+	//For getting entity
+	std::vector<std::string> getAllEntity(qps::TokenType returnType);
+
+	//For Uses Relationship
+	/*bool getUses(int lineNumber, std::string var);
+	std::vector<std::string> getUsesByVar(TokenType returnType, std::string var);
+	std::vector<std::string> getUsesbyStmt(int lineNumber);
+	std::vector<std::string> getAllModify(TokenType returnType);*/
+
+private:
+	PKB* pkb;
 };
