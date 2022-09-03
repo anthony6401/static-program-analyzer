737acf5b3cd790f47ca3a8c482bed6d8f853191d
Kacalamaka <heinrich2001.ang@gmail.com>
2022-09-03 22:44:44 +0800

    make all variable static

--- Team17/Code17/src/unit_testing/src/pkb/EntityObject.h
+++ Team17/Code17/src/unit_testing/src/pkb/EntityObject.h
@@ -13,37 +13,37 @@
 #include "models/Entity/VariableEntity.h"
 #include "models/Entity/WhileEntity.h"
 
-std::string value_one = "value_one";
-std::string value_two = "value_two";
-std::string value_three = "value_three";
+static std::string value_one = "value_one";
+static std::string value_two = "value_two";
+static std::string value_three = "value_three";
 
-Entity* entity = new Entity(value_one);
-Entity* assignEntity = new AssignEntity(value_one);
-Entity* callEntity = new CallEntity(value_one);
-Entity* constantEntity = new ConstantEntity(value_one);
-Entity* ifEntity = new IfEntity(value_one);
-Entity* printEntity = new PrintEntity(value_one);
-Entity* procedureEntity = new ProcedureEntity(value_one);
-Entity* readEntity = new ReadEntity(value_one);
-Entity* variableEntity = new VariableEntity(value_one);
-Entity* whileEntity = new WhileEntity(value_one);
+static Entity* entity = new Entity(value_one);
+static Entity* assignEntity = new AssignEntity(value_one);
+static Entity* callEntity = new CallEntity(value_one);
+static Entity* constantEntity = new ConstantEntity(value_one);
+static Entity* ifEntity = new IfEntity(value_one);
+static Entity* printEntity = new PrintEntity(value_one);
+static Entity* procedureEntity = new ProcedureEntity(value_one);
+static Entity* readEntity = new ReadEntity(value_one);
+static Entity* variableEntity = new VariableEntity(value_one);
+static Entity* whileEntity = new WhileEntity(value_one);
 
-Entity* assignEntityTwo = new AssignEntity(value_two);
-Entity* callEntityTwo = new CallEntity(value_two);
-Entity* constantEntityTwo = new ConstantEntity(value_two);
-Entity* ifEntityTwo = new IfEntity(value_two);
-Entity* printEntityTwo = new PrintEntity(value_two);
-Entity* procedureEntityTwo = new ProcedureEntity(value_two);
-Entity* readEntityTwo = new ReadEntity(value_two);
-Entity* variableEntityTwo = new VariableEntity(value_two);
-Entity* whileEntityTwo = new WhileEntity(value_two);
+static Entity* assignEntityTwo = new AssignEntity(value_two);
+static Entity* callEntityTwo = new CallEntity(value_two);
+static Entity* constantEntityTwo = new ConstantEntity(value_two);
+static Entity* ifEntityTwo = new IfEntity(value_two);
+static Entity* printEntityTwo = new PrintEntity(value_two);
+static Entity* procedureEntityTwo = new ProcedureEntity(value_two);
+static Entity* readEntityTwo = new ReadEntity(value_two);
+static Entity* variableEntityTwo = new VariableEntity(value_two);
+static Entity* whileEntityTwo = new WhileEntity(value_two);
 
-Entity* assignEntityThree = new AssignEntity(value_three);
-Entity* callEntityThree = new CallEntity(value_three);
-Entity* constantEntityThree = new ConstantEntity(value_three);
-Entity* ifEntityThree = new IfEntity(value_three);
-Entity* printEntityThree = new PrintEntity(value_three);
-Entity* procedureEntityThree = new ProcedureEntity(value_three);
-Entity* readEntityThree = new ReadEntity(value_three);
-Entity* variableEntityThree = new VariableEntity(value_three);
-Entity* whileEntityThree = new WhileEntity(value_three);
+static Entity* assignEntityThree = new AssignEntity(value_three);
+static Entity* callEntityThree = new CallEntity(value_three);
+static Entity* constantEntityThree = new ConstantEntity(value_three);
+static Entity* ifEntityThree = new IfEntity(value_three);
+static Entity* printEntityThree = new PrintEntity(value_three);
+static Entity* procedureEntityThree = new ProcedureEntity(value_three);
+static Entity* readEntityThree = new ReadEntity(value_three);
+static Entity* variableEntityThree = new VariableEntity(value_three);
+static Entity* whileEntityThree = new WhileEntity(value_three);
