f328bf77fdbbcfdea76f037d7fb994d13f61ea8a
Kacalamaka <heinrich2001.ang@gmail.com>
2022-09-03 22:26:42 +0800

    updated storeRelationship() to store entity first!

--- Team17/Code17/src/spa/src/components/pkb/pkb.cpp
+++ Team17/Code17/src/spa/src/components/pkb/pkb.cpp
@@ -6,15 +6,25 @@
 
 #include "../qps/query_preprocessor/query_tokenizer/TokenType.h"
 
+#include <unordered_set>
+
 PKB::PKB() {
 	entityManager = new EntityManager();
 };
 
-std::vector<std::string> PKB::getAllEntity(qps::TokenType returnType) {
+std::unordered_set<std::string> PKB::getAllEntity(qps::TokenType returnType) {
 	return entityManager->getAllEntity(returnType);
 }
 
 bool PKB::storeRelationship(Relationship* rel) {
-	//to be implemented
-	return 0;
+	bool ret = true;
+
+	//Store entity first if haven't
+	ret = ret && entityManager->storeEntity(rel->getLeftEntity());
+	ret = ret && entityManager->storeEntity(rel->getRightEntity());
+
+	//TODO need to store relationship soon!!
+	//For now just store entity from the given relationship
+
+	return ret;
 }
--- Team17/Code17/src/spa/src/components/pkb/pkb.h
+++ Team17/Code17/src/spa/src/components/pkb/pkb.h
@@ -1,18 +1,21 @@
 #pragma once
 
 #include "../../models/Relationship/Relationship.h"
+#include  "models/Entity/Entity.h"
 
 #include "./manager/EntityManager.h"
 
 #include "../qps/query_preprocessor/query_tokenizer/TokenType.h"
 
+#include <unordered_set>
+
 class PKB {
 public:
 	PKB();
 	//For getting entity
-	std::vector<std::string> getAllEntity(qps::TokenType returnType);
+	std::unordered_set<std::string> getAllEntity(qps::TokenType returnType);
 
-	//For getting relationship
+	//For storing relationship
 	bool storeRelationship(Relationship* rel);
 private:
 	//Add managers
