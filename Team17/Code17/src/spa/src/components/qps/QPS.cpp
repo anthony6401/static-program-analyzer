d219be059c28efc0c3e96a24b9ecc137c9a2d120
Andrea Tan <andreaxtan611@gmail.com>
2022-09-04 21:19:04 +0800

    Update qps.cpp file to integrate parser and evaluator

--- Team17/Code17/src/spa/src/components/qps/QPS.cpp
+++ Team17/Code17/src/spa/src/components/qps/QPS.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <components/qps/query_preprocessor/query_tokenizer/Tokenizer.h>
 #include <components/qps/query_preprocessor/query_tokenizer/TokenObject.h>
+#include <components/qps/query_preprocessor/query_parser/Parser.h>
 #include <components/qps/query_evaluator/Evaluator.h>
 #include "QPS.h"
 #include "components/qps/abstract_query_object/QueryObject.h"
@@ -12,11 +13,13 @@ QueryObject QPS::tokenizeAndParseQuery(std::string query) {
     Tokenizer tokenizer = *new Tokenizer();
     std::vector<TokenObject> tokenList = tokenizer.tokenize(query);
     // Parse query and check semantics
-    return QueryObject();
+    Parser parser = Parser(tokenList);
+    QueryObject parsedQuery = parser.parse();
+    return parsedQuery;
 }
 
 void QPS::obtainQueryResult(std::string query, std::list<std::string> &results) {
-    QueryObject queryObject = tokenizeAndParseQuery(query);
-    Evaluator::evaluateQuery(queryObject, results);
+    QueryObject parsedQuery = tokenizeAndParseQuery(query);
+    Evaluator::evaluateQuery(parsedQuery, results);
 }
 
