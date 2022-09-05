caeb44f3bf6866d34abe032d101bcefc7c8929eb
hyngkng <hyngkng@gmail.com>
2022-09-05 09:48:36 +0800

    Change tests to comments

--- Team17/Code17/src/unit_testing/src/sp/extractor/ExtractorTest.cpp
+++ Team17/Code17/src/unit_testing/src/sp/extractor/ExtractorTest.cpp
@@ -4,33 +4,36 @@
 #include <catch.hpp>
 
 TEST_CASE("extract read") {
-	Extractor* extractorTest = new Extractor();
+	// to be implemented
+	
+	// Extractor* extractorTest = new Extractor();
+	// std::vector<std::string> test_tokens;
+	// test_tokens.push_back("test");
+	// test_tokens.push_back(";");
 
-	std::vector<std::string> test_tokens;
-	test_tokens.push_back("test");
-	test_tokens.push_back(";");
+	// std::string code = "read test;";
+	// SimpleToken simpleToken = SimpleToken(TokenType::TREAD, code,
+	// 	1, SimpleParser::parseRead);
+	// extractorTest->extractRead(simpleToken, test_tokens);
 
-	std::string code = "read test;";
-	SimpleToken simpleToken = SimpleToken(TokenType::TREAD, code,
-		1, SimpleParser::parseRead);
-	extractorTest->extractRead(simpleToken, test_tokens);
-
-	PKB* test_result = extractorTest->client->getPKB();
+	// PKB* test_result = extractorTest->client->getPKB();
 
 }
 
 TEST_CASE("extract print") {
-	Extractor* extractorTest = new Extractor();
+	// to be implemented
+	// 
+	// Extractor* extractorTest = new Extractor();
 
-	std::vector<std::string> test_tokens;
-	test_tokens.push_back("test");
-	test_tokens.push_back(";");
+	// std::vector<std::string> test_tokens;
+	// test_tokens.push_back("test");
+	// test_tokens.push_back(";");
 
-	std::string code = "print test;";
-	SimpleToken simpleToken = SimpleToken(TokenType::TPRINT, code,
-		1, SimpleParser::parseRead);
-	extractorTest->extractRead(simpleToken, test_tokens);
+	// std::string code = "print test;";
+	// SimpleToken simpleToken = SimpleToken(TokenType::TPRINT, code,
+	//	1, SimpleParser::parseRead);
+	// extractorTest->extractRead(simpleToken, test_tokens);
 
-	PKB* test_result = extractorTest->client->getPKB();
+	// PKB* test_result = extractorTest->client->getPKB();
 
 }
