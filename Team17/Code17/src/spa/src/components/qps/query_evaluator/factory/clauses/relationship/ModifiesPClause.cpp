292baa24e40833b9f98818100f266cbe485f9e54
Andrea Tan <andreaxtan611@gmail.com>
2022-09-08 12:21:37 +0800

    Add ModifiesPClause constructor and fields

--- Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/ModifiesPClause.cpp
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/ModifiesPClause.cpp
@@ -1,5 +1,12 @@
-//
-// Created by ANDREA TAN on 8/9/22.
-//
-
 #include "ModifiesPClause.h"
+
+ModifiesPClause::ModifiesPClause(TokenObject left, TokenObject right, Select synonym,
+                                 std::unordered_map<std::string, TokenType> synonymToDesignEntityMap,
+                                 QPSClient qpsClient) : left(left), right(right), synonym(synonym),
+                                                        synonymToDesignEntityMap(synonymToDesignEntityMap), qpsClient(qpsClient) {};
+
+std::unordered_set<std::string> ModifiesPClause::evaluateClause() {
+    std::unordered_set<std::string> result;
+    return result;
+}
+
--- Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/ModifiesPClause.h
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/ModifiesPClause.h
@@ -1,8 +1,23 @@
-//
-// Created by ANDREA TAN on 8/9/22.
-//
-
 #ifndef SPA_MODIFIESPCLAUSE_H
 #define SPA_MODIFIESPCLAUSE_H
+#include "components/qps/query_preprocessor/query_tokenizer/TokenObject.h"
+#include "components/qps/query_evaluator/factory/interface/Clause.h"
+#include "components/qps/abstract_query_object/Select.h"
+#include "components/pkb/clients/QPSClient.h"
+#include <unordered_map>
+#include <list>
 
+class ModifiesPClause : public Clause {
+private:
+    std::unordered_map<std::string, TokenType> synonymToDesignEntityMap;
+    Select synonym;
+    QPSClient qpsClient;
+    TokenObject left;
+    TokenObject right;
+
+public:
+    ModifiesPClause(TokenObject left, TokenObject right, Select synonym,
+                    std::unordered_map<std::string, TokenType> synonymToDesignEntityMap, QPSClient qpsClient);
+    std::unordered_set<std::string> evaluateClause() override;
+};
 #endif //SPA_MODIFIESPCLAUSE_H
