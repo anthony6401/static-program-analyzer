acff34c2da8ec478590fb4ebe654086f94f697ae
Andrea Tan <andreaxtan611@gmail.com>
2022-09-11 23:29:57 +0800

    Add followsclause base methods

--- Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/FollowsClause.cpp
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/FollowsClause.cpp
@@ -1,5 +1,70 @@
-//
-// Created by ANDREA TAN on 11/9/22.
-//
-
 #include "FollowsClause.h"
+
+FollowsClause::FollowsClause(TokenObject left, TokenObject right, Select synonym,
+                             std::unordered_map<std::string, DesignEntity> synonymToDesignEntityMap,
+                             QPSClient qpsClient) : left(left), right(right), synonym(synonym),
+                                                    synonymToDesignEntityMap(synonymToDesignEntityMap), qpsClient(qpsClient) {}
+
+RawResult FollowsClause::evaluateClause() {
+    TokenType leftType = left.getTokenType();
+    TokenType rightType = right.getTokenType();
+    if (leftType == TokenType::SYNONYM && rightType == TokenType::SYNONYM) {
+        return FollowsClause::evaluateSynonymSynonym();
+    } else if (leftType == TokenType::SYNONYM && rightType == TokenType::WILDCARD) {
+        return FollowsClause::evaluateSynonymWildcard();
+    } else if (leftType == TokenType::SYNONYM && rightType == TokenType::INTEGER) {
+        return FollowsClause::evaluateSynonymInteger();
+    } else if (leftType == TokenType::INTEGER && rightType == TokenType::SYNONYM) {
+        return FollowsClause::evaluateIntegerSynonym();
+    } else if (leftType == TokenType::INTEGER && rightType == TokenType::WILDCARD) {
+        return FollowsClause::evaluateIntegerWildcard();
+    } else if (leftType == TokenType::INTEGER && rightType == TokenType::INTEGER) {
+        return FollowsClause::evaluateIntegerInteger();
+    } else if (leftType == TokenType::WILDCARD && rightType == TokenType::SYNONYM) {
+        return FollowsClause::evaluateWildcardSynonym();
+    } else if (leftType == TokenType::WILDCARD && rightType == TokenType::WILDCARD) {
+        return FollowsClause::evaluateWildcardWildcard();
+    } else if (leftType == TokenType::WILDCARD && rightType == TokenType::INTEGER) {
+        return FollowsClause::evaluateWildcardInteger();
+    } else {
+        return {};
+    }
+}
+
+RawResult FollowsClause::evaluateSynonymSynonym() {
+    return {};
+}
+
+RawResult FollowsClause::evaluateSynonymWildcard() {
+    return {};
+}
+
+RawResult FollowsClause::evaluateSynonymInteger() {
+    return {};
+}
+
+RawResult FollowsClause::evaluateIntegerSynonym() {
+    return {};
+}
+
+RawResult FollowsClause::evaluateIntegerWildcard() {
+    return {};
+}
+
+RawResult FollowsClause::evaluateIntegerInteger() {
+    return {};
+}
+
+RawResult FollowsClause::evaluateWildcardSynonym() {
+    return {};
+}
+
+RawResult FollowsClause::evaluateWildcardWildcard() {
+    return {};
+}
+
+RawResult FollowsClause::evaluateWildcardInteger() {
+    return {};
+}
+
+
--- Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/FollowsClause.h
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/FollowsClause.h
@@ -1,8 +1,33 @@
-//
-// Created by ANDREA TAN on 11/9/22.
-//
-
 #ifndef SPA_FOLLOWSCLAUSE_H
 #define SPA_FOLLOWSCLAUSE_H
+#include "components/qps/query_preprocessor/query_tokenizer/TokenObject.h"
+#include "components/qps/query_evaluator/factory/interface/Clause.h"
+#include "components/qps/abstract_query_object/Select.h"
+#include "components/pkb/clients/QPSClient.h"
+#include <unordered_map>
+#include <list>
+
+class FollowsClause : public Clause {
+private:
+    std::unordered_map<std::string, DesignEntity> synonymToDesignEntityMap;
+    Select synonym;
+    QPSClient qpsClient;
+    TokenObject left;
+    TokenObject right;
+
+public:
+    FollowsClause(TokenObject left, TokenObject right, Select synonym,
+    std::unordered_map<std::string, DesignEntity> synonymToDesignEntityMap, QPSClient qpsClient);
+    RawResult evaluateClause() override;
+    RawResult evaluateSynonymSynonym();
+    RawResult evaluateSynonymWildcard();
+    RawResult evaluateSynonymInteger();
+    RawResult evaluateIntegerSynonym();
+    RawResult evaluateIntegerWildcard();
+    RawResult evaluateIntegerInteger();
+    RawResult evaluateWildcardSynonym();
+    RawResult evaluateWildcardWildcard();
+    RawResult evaluateWildcardInteger();
+};
 
 #endif //SPA_FOLLOWSCLAUSE_H
