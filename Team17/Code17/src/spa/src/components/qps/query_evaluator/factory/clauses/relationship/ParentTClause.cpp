de9e68696a3137c2cc25256d1bd8260519318ffd
Andrea Tan <andreaxtan611@gmail.com>
2022-09-12 00:36:28 +0800

    Add parentT base classes

---
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/ParentTClause.cpp
@@ -0,0 +1,72 @@
+#include "ParentTClause.h"
+
+ParentTClause::ParentTClause(TokenObject left, TokenObject right, Select synonym,
+                               std::unordered_map<std::string, DesignEntity> synonymToDesignEntityMap,
+                               QPSClient qpsClient) : left(left), right(right), synonym(synonym),
+                                                      synonymToDesignEntityMap(synonymToDesignEntityMap), qpsClient(qpsClient) {}
+
+RawResult ParentTClause::evaluateClause() {
+    TokenType leftType = left.getTokenType();
+    TokenType rightType = right.getTokenType();
+    if (leftType == TokenType::SYNONYM && rightType == TokenType::SYNONYM) {
+        return ParentTClause::evaluateSynonymSynonym();
+    } else if (leftType == TokenType::SYNONYM && rightType == TokenType::WILDCARD) {
+        return ParentTClause::evaluateSynonymWildcard();
+    } else if (leftType == TokenType::SYNONYM && rightType == TokenType::INTEGER) {
+        return ParentTClause::evaluateSynonymInteger();
+    } else if (leftType == TokenType::INTEGER && rightType == TokenType::SYNONYM) {
+        return ParentTClause::evaluateIntegerSynonym();
+    } else if (leftType == TokenType::INTEGER && rightType == TokenType::WILDCARD) {
+        return ParentTClause::evaluateIntegerWildcard();
+    } else if (leftType == TokenType::INTEGER && rightType == TokenType::INTEGER) {
+        return ParentTClause::evaluateIntegerInteger();
+    } else if (leftType == TokenType::WILDCARD && rightType == TokenType::SYNONYM) {
+        return ParentTClause::evaluateWildcardSynonym();
+    } else if (leftType == TokenType::WILDCARD && rightType == TokenType::WILDCARD) {
+        return ParentTClause::evaluateWildcardWildcard();
+    } else if (leftType == TokenType::WILDCARD && rightType == TokenType::INTEGER) {
+        return ParentTClause::evaluateWildcardInteger();
+    } else {
+        return {};
+    }
+}
+
+TokenType getRelationshipType() {
+    return TokenType::PARENT_T;
+}
+
+RawResult ParentTClause::evaluateSynonymSynonym() {
+    return {};
+}
+
+RawResult ParentTClause::evaluateSynonymWildcard() {
+    return {};
+}
+
+RawResult ParentTClause::evaluateSynonymInteger() {
+    return {};
+}
+
+RawResult ParentTClause::evaluateIntegerSynonym() {
+    return {};
+}
+
+RawResult ParentTClause::evaluateIntegerWildcard() {
+    return {};
+}
+
+RawResult ParentTClause::evaluateIntegerInteger() {
+    return {};
+}
+
+RawResult ParentTClause::evaluateWildcardSynonym() {
+    return {};
+}
+
+RawResult ParentTClause::evaluateWildcardWildcard() {
+    return {};
+}
+
+RawResult ParentTClause::evaluateWildcardInteger() {
+    return {};
+}
