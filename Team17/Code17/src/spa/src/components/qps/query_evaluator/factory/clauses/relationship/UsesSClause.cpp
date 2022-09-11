0e088d2463d54a265643c0370b21c4006859819c
Andrea Tan <andreaxtan611@gmail.com>
2022-09-11 22:02:49 +0800

    Add usesS base methods

---
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/UsesSClause.cpp
@@ -0,0 +1,67 @@
+#include "UsesSClause.h"
+
+UsesSClause::UsesSClause(TokenObject left, TokenObject right, Select synonym,
+                         std::unordered_map<std::string, DesignEntity> synonymToDesignEntityMap, QPSClient qpsClient)
+                         : left(left), right(right), synonym(synonym), synonymToDesignEntityMap(synonymToDesignEntityMap)
+                         , qpsClient(qpsClient) {};
+
+
+RawResult UsesSClause::evaluateClause() {
+    TokenType leftType = left.getTokenType();
+    TokenType rightType = right.getTokenType();
+    if (leftType == TokenType::SYNONYM && rightType == TokenType::SYNONYM) {
+        return UsesSClause::evaluateSynonymSynonym();
+    } else if (leftType == TokenType::SYNONYM && rightType == TokenType::WILDCARD) {
+        return UsesSClause::evaluateSynonymWildcard();
+    } else if (leftType == TokenType::SYNONYM && rightType == TokenType::NAME_WITH_QUOTATION) {
+        return UsesSClause::evaluateSynonymNameQuotes();
+    } else if (leftType == TokenType::INTEGER && rightType == TokenType::SYNONYM) {
+        return UsesSClause::evaluateIntegerSynonym();
+    } else if (leftType == TokenType::INTEGER && rightType == TokenType::WILDCARD) {
+        return UsesSClause::evaluateIntegerWildcard();
+    } else if (leftType == TokenType::INTEGER && rightType == TokenType::NAME_WITH_QUOTATION) {
+        return UsesSClause::evaluateIntegerNameQuotes();
+    } else {
+        return {};
+    }
+}
+
+RawResult UsesSClause::evaluateSynonymSynonym() {
+    DesignEntity stmtType = synonymToDesignEntityMap[left.getValue()];
+    DesignEntity rightType = synonymToDesignEntityMap[right.getValue()];
+    // API CALL
+    return {};
+}
+
+RawResult UsesSClause::evaluateSynonymWildcard() {
+    DesignEntity stmtType = synonymToDesignEntityMap[left.getValue()];
+    // API CALL
+    return {};
+}
+
+RawResult UsesSClause::evaluateSynonymNameQuotes() {
+    DesignEntity stmtType = synonymToDesignEntityMap[left.getValue()];
+    // API CALL
+    return {};
+}
+
+RawResult UsesSClause::evaluateIntegerSynonym() {
+    DesignEntity rightType = synonymToDesignEntityMap[right.getValue()];
+    // API CALL
+    return {};
+}
+
+RawResult UsesSClause::evaluateIntegerWildcard() {
+    // API CALL
+    return {};
+}
+
+RawResult UsesSClause::evaluateIntegerNameQuotes() {
+    // API CALL
+    return {};
+}
+
+
+TokenType UsesSClause::getRelationshipType() {
+    return TokenType::USES;
+}
