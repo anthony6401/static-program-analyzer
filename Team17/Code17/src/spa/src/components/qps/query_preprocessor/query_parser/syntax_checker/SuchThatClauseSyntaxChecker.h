e81b56b44550fb7ae5cacba2312d63648b09e3b4
ChooJiaXin <e0426255@u.nus.edu>
2022-09-11 16:09:46 +0800

    Remove commented out reference

--- Team17/Code17/src/spa/src/components/qps/query_preprocessor/query_parser/syntax_checker/SuchThatClauseSyntaxChecker.h
+++ Team17/Code17/src/spa/src/components/qps/query_preprocessor/query_parser/syntax_checker/SuchThatClauseSyntaxChecker.h
@@ -11,19 +11,12 @@ using namespace qps;
 class SuchThatClauseSyntaxChecker : public SyntaxChecker {
 private:
     std::stack<TokenType> suchThatSyntax;
+    bool hasValidFollowsParentSyntax(std::vector<TokenObject> relationshipClauseTokens);
+    bool hasValidUsersModifiesSyntax(std::vector<TokenObject> relationshipClauseTokens);
 public:
     SuchThatClauseSyntaxChecker();
     ~SuchThatClauseSyntaxChecker();
     bool isSyntacticallyCorrect(std::vector<TokenObject> tokenizedClause);
-    bool hasValidFollowsParentSyntax(std::vector<TokenObject> relationshipClauseTokens);
-    bool hasValidUsersModifiesSyntax(std::vector<TokenObject> relationshipClauseTokens);
-
-    //{TokenType::FOLLOWS, std::vector<TokenType>{TokenType::FOLLOWS, TokenType::OPEN_BRACKET, TokenType::STMT_STMT_PARAMS, TokenType::CLOSED_BRACKET}},
-    //{ TokenType::FOLLOWS_T, std::vector<TokenType>{TokenType::FOLLOWS_T, TokenType::OPEN_BRACKET, TokenType::STMT_STMT_PARAMS, TokenType::CLOSED_BRACKET} },
-    //{ TokenType::PARENT, std::vector<TokenType>{TokenType::PARENT, TokenType::OPEN_BRACKET, TokenType::STMT_STMT_PARAMS, TokenType::CLOSED_BRACKET} },
-    //{ TokenType::PARENT_T, std::vector<TokenType>{TokenType::PARENT_T, TokenType::OPEN_BRACKET, TokenType::STMT_STMT_PARAMS, TokenType::CLOSED_BRACKET} },
-    //{ TokenType::USES, std::vector<TokenType>{TokenType::SYNONYM, TokenType::WILDCARD, TokenType::INTEGER} },
-    //{ TokenType::MODIFIES, std::vector<TokenType>{TokenType::SYNONYM, TokenType::WILDCARD, TokenType::INTEGER} },
 };
 
 #endif //INC_22S1_CP_SPA_TEAM_17_SUCH_THAT_SYNTAX_CHECKER_H   
