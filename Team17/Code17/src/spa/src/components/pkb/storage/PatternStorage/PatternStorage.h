b0db076d029bf2607585e0c8abcfa0696ed5bf6d
Kacalamaka <heinrich2001.ang@gmail.com>
2022-09-12 20:54:04 +0800

    Added namespace kb to differentiate different Pattern class

--- Team17/Code17/src/spa/src/components/pkb/storage/PatternStorage/PatternStorage.h
+++ Team17/Code17/src/spa/src/components/pkb/storage/PatternStorage/PatternStorage.h
@@ -5,11 +5,12 @@
 #include "models/Entity/DesignEntity.h"
 #include "../../../qps/query_preprocessor/query_tokenizer/TokenObject.h"
 
+
 class PatternStorage {
 
 public:
 	PatternStorage();
-	virtual bool storePattern(Pattern* pattern) = 0;
+	virtual bool storePattern(kb::Pattern* pattern) = 0;
 	virtual std::unordered_set<std::string> getPattern(DesignEntity designEntity, TokenObject firstArgument, TokenObject secondArgument) = 0;
 	virtual std::vector<std::pair<std::string, std::string>> getPatternPair(DesignEntity designEntity, TokenObject secondArgument) = 0;
 };
