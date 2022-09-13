9da1874621949fc151737c8abe0ca269ed09b410
hyngkng <hyngkng@gmail.com>
2022-09-13 17:37:41 +0800

    Fix build issue

--- Team17/Code17/src/spa/src/components/sp/extractor/ParentExtractor.cpp
+++ Team17/Code17/src/spa/src/components/sp/extractor/ParentExtractor.cpp
@@ -1,9 +1,9 @@
 #include "ParentExtractor.h"
 
-void ParentExtractor::extractParent(Extractor extractor, SimpleToken, parent, std::vector<SimpleToken> seriesOfStmts) {
+void ParentExtractor::extractParent(Extractor extractor, SimpleToken parent, std::vector<SimpleToken> seriesOfStmts) {
 	// code here
 }
 
-void ParentExtractor::extractParentT(Extractor extractor, SimpleToken, parent, std::vector<SimpleToken> seriesOfStmts) {
+void ParentExtractor::extractParentT(Extractor extractor, SimpleToken parent, std::vector<SimpleToken> seriesOfStmts) {
 	// code here
 }
