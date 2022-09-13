d24669c3509984eedfed15f68bab58f1ac8fb49e
hyngkng <hyngkng@gmail.com>
2022-09-14 00:14:20 +0800

    Implement ParentExtractor

--- Team17/Code17/src/spa/src/components/sp/extractor/ParentExtractor.h
+++ Team17/Code17/src/spa/src/components/sp/extractor/ParentExtractor.h
@@ -9,4 +9,7 @@ class ParentExtractor {
 public:
 	void static extractParent(Extractor extractor, SimpleToken parent, std::vector<SimpleToken> seriesOfStmts);
 	void static extractParentT(Extractor extractor, SimpleToken parent, std::vector<SimpleToken> seriesOfStmts);
+
+private:
+	static Entity* generateEntity(SimpleToken token);
 };
