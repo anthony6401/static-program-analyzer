50553066b78c1eecf66e752d94259d03ba0fb829
hyngkng <hyngkng@gmail.com>
2022-09-14 15:32:26 +0800

    Fix build issue in patternextractor

--- Team17/Code17/src/spa/src/components/sp/extractor/PatternExtractor.cpp
+++ Team17/Code17/src/spa/src/components/sp/extractor/PatternExtractor.cpp
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-AssignPattern PatternExtractor::extractPattern(Extractor extractor, SimpleToken variable, SimpleToken expression) {
+void PatternExtractor::extractPattern(Extractor extractor, SimpleToken variable, SimpleToken expression) {
 	std::string lineNum = std::to_string(variable.statementNumber);
 	std::string firstValue = variable.value;
 	std::string secondValue = expression.value;
