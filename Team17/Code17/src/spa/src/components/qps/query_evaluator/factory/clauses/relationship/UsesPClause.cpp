4571d791732a7f72037ff729ccd0ad982fdf96c9
Andrea Tan <andreaxtan611@gmail.com>
2022-09-15 21:13:16 +0800

    Add api for UsesP

--- Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/UsesPClause.cpp
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/UsesPClause.cpp
@@ -48,33 +48,78 @@ std::set<std::string> UsesPClause::getAllSynonyms() {
     return synonyms;
 }
 
-TokenType UsesPClause::getRelationshipType() {
-    return TokenType::USES;
+std::vector<std::pair<std::string, std::string>> UsesPClause::processMapToVectorPair(std::unordered_map<std::string, std::unordered_set<std::string>> results) {
+    std::vector<std::pair<std::string, std::string>> processedResult;
+    for (auto entry : results) {
+        std::string firstSynonym = entry.first;
+        std::unordered_set<std::string> secondSynonymList = entry.second;
+        for (auto secondSynonym : secondSynonymList) {
+            std::pair<std::string, std::string> newPair = {firstSynonym, secondSynonym};
+            processedResult.emplace_back(newPair);
+        }
+    }
+    return processedResult;
+}
+
+std::unordered_set<std::string> UsesPClause::processMapToSet(std::unordered_map<std::string, std::unordered_set<std::string>> results) {
+    std::unordered_set<std::string> processedResult;
+    for (auto entry : results) {
+        std::string firstSynonym = entry.first;
+        processedResult.insert(firstSynonym);
+    }
+    return processedResult;
+}
+
+RelationshipType UsesPClause::getRelationshipType() {
+    return RelationshipType::USES;
 }
 
 RawResult UsesPClause::evaluateSynonymSynonym() {
-    std::cout << "----- UsesP: synonym synonym -------" << std::endl;
-    return {"p", "v", {{"main", "x"}}};
+    DesignEntity stmtType = synonymToDesignEntityMap[left.getValue()]; // Procedure
+    DesignEntity rightType = synonymToDesignEntityMap[right.getValue()];
+    std::string leftValue = left.getValue();
+    std::string rightValue = right.getValue();
+    std::unordered_map<std::string, std::unordered_set<std::string>> results = qpsClient.getAllRelationship(getRelationshipType(), stmtType, rightType);
+    std::vector<std::pair<std::string, std::string>> processedMap = UsesPClause::processMapToVectorPair(results); // {{"1", "x"}, {"2", "y"}}
+    return {leftValue, rightValue, processedMap};
 }
 
 RawResult UsesPClause::evaluateSynonymWildcard() {
-    return {};
+    DesignEntity stmtType = synonymToDesignEntityMap[left.getValue()]; // Procedure
+    DesignEntity rightType = DesignEntity::VARIABLE;
+    std::string leftValue = left.getValue();
+    std::unordered_map<std::string, std::unordered_set<std::string>> results = qpsClient.getAllRelationship(getRelationshipType(), stmtType, rightType);
+    std::unordered_set<std::string> processedMap = UsesPClause::processMapToSet(results);
+    return {leftValue, processedMap};
 }
 
 RawResult UsesPClause::evaluateSynonymNameQuotes() {
-    return {};
+    DesignEntity stmtType = synonymToDesignEntityMap[left.getValue()];
+    std::string leftValue = left.getValue();
+    std::unordered_set<std::string> results = qpsClient.getRelationshipBySecond(getRelationshipType(), stmtType, right);
+    return {leftValue, results};
 }
 
 RawResult UsesPClause::evaluateNameQuotesSynonym() {
-    return {};
+    std::string rightValue = right.getValue();
+    DesignEntity rightType = synonymToDesignEntityMap[right.getValue()];
+    std::unordered_set<std::string> results = qpsClient.getRelationshipByFirst(getRelationshipType(), left, rightType);
+    return {rightValue, results};
 }
 
 RawResult UsesPClause::evaluateNameQuotesWildcard() {
-    return {};
+    DesignEntity rightType = DesignEntity::VARIABLE;
+    std::unordered_set<std::string> results = qpsClient.getRelationshipByFirst(getRelationshipType(), left, rightType);
+    bool booleanResult = !results.empty();
+    // {1,2,3} -> boolean result = true
+    return {booleanResult}; //true
 }
 
 RawResult UsesPClause::evaluateNameQuotesNameQuotes() {
-    return {};
+    // Returns boolean
+    bool result = qpsClient.getRelationship(getRelationshipType(), left, right);
+    // result = true -> setIsFalseResult(true) -> isFalseResult = false
+    return {result};
 }
 
 
