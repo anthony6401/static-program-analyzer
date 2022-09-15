6f8914decc31f91c757c69dfe6827f400757d230
Andrea Tan <andreaxtan611@gmail.com>
2022-09-15 20:49:30 +0800

    Add api for modifiesP

--- Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/ModifiesPClause.cpp
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/relationship/ModifiesPClause.cpp
@@ -47,31 +47,77 @@ std::set<std::string> ModifiesPClause::getAllSynonyms() {
     return synonyms;
 }
 
-TokenType ModifiesPClause::getRelationshipType() {
-    return TokenType::MODIFIES;
+std::vector<std::pair<std::string, std::string>> ModifiesPClause::processMapToVectorPair(std::unordered_map<std::string, std::unordered_set<std::string>> results) {
+    std::vector<std::pair<std::string, std::string>> processedResult;
+    for (auto entry : results) {
+        std::string firstSynonym = entry.first;
+        std::unordered_set<std::string> secondSynonymList = entry.second;
+        for (auto secondSynonym : secondSynonymList) {
+            std::pair<std::string, std::string> newPair = {firstSynonym, secondSynonym};
+            processedResult.emplace_back(newPair);
+        }
+    }
+    return processedResult;
+}
+
+std::unordered_set<std::string> ModifiesPClause::processMapToSet(std::unordered_map<std::string, std::unordered_set<std::string>> results) {
+    std::unordered_set<std::string> processedResult;
+    for (auto entry : results) {
+        std::string firstSynonym = entry.first;
+        processedResult.insert(firstSynonym);
+    }
+    return processedResult;
+}
+
+RelationshipType ModifiesPClause::getRelationshipType() {
+    return RelationshipType::MODIFIES;
 }
 
 RawResult ModifiesPClause::evaluateSynonymSynonym() {
-    return {};
+    DesignEntity stmtType = synonymToDesignEntityMap[left.getValue()];
+    DesignEntity rightType = synonymToDesignEntityMap[right.getValue()];
+    std::string leftValue = left.getValue();
+    std::string rightValue = right.getValue();
+    std::unordered_map<std::string, std::unordered_set<std::string>> results = qpsClient.getAllRelationship(getRelationshipType(), stmtType, rightType);
+    std::vector<std::pair<std::string, std::string>> processedMap = ModifiesPClause::processMapToVectorPair(results); // {{"1", "x"}, {"2", "y"}}
+    return {leftValue, rightValue, processedMap};
 }
 
 RawResult ModifiesPClause::evaluateSynonymWildcard() {
-    return {};
+    DesignEntity stmtType = synonymToDesignEntityMap[left.getValue()];
+    DesignEntity rightType = DesignEntity::VARIABLE;
+    std::string leftValue = left.getValue();
+    std::unordered_map<std::string, std::unordered_set<std::string>> results = qpsClient.getAllRelationship(getRelationshipType(), stmtType, rightType);
+    std::unordered_set<std::string> processedMap = ModifiesPClause::processMapToSet(results);
+    return {leftValue, processedMap};
 }
 
 RawResult ModifiesPClause::evaluateSynonymNameQuotes() {
-    return {};
+    DesignEntity stmtType = synonymToDesignEntityMap[left.getValue()];
+    std::string leftValue = left.getValue();
+    std::unordered_set<std::string> results = qpsClient.getRelationshipBySecond(getRelationshipType(), stmtType, right);
+    return {leftValue, results};
 }
 
 RawResult ModifiesPClause::evaluateNameQuotesSynonym() {
-    return {};
+    std::string rightValue = right.getValue();
+    DesignEntity rightType = synonymToDesignEntityMap[right.getValue()];
+    std::unordered_set<std::string> results = qpsClient.getRelationshipByFirst(getRelationshipType(), left, rightType);
+    return {rightValue, results};
 }
 
 RawResult ModifiesPClause::evaluateNameQuotesWildcard() {
-    return {};
+    DesignEntity rightType = DesignEntity::VARIABLE;
+    std::unordered_set<std::string> results = qpsClient.getRelationshipByFirst(getRelationshipType(), left, rightType);
+    bool booleanResult = !results.empty();
+    // {1,2,3} -> boolean result = true
+    return {booleanResult}; //true
 }
 
 RawResult ModifiesPClause::evaluateNameQuotesNameQuotes() {
-    return {};
+    // Returns boolean
+    bool result = qpsClient.getRelationship(getRelationshipType(), left, right);
+    // result = true -> setIsFalseResult(true) -> isFalseResult = false
+    return {result};
 }
 
