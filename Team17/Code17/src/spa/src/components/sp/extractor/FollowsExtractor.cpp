770ce1d2d881aba94993b882e4116f4bc94b36fc
hyngkng <hyngkng@gmail.com>
2022-09-18 00:31:09 +0800

    Debug follows, followsT, parent and parentT

--- Team17/Code17/src/spa/src/components/sp/extractor/FollowsExtractor.cpp
+++ Team17/Code17/src/spa/src/components/sp/extractor/FollowsExtractor.cpp
@@ -54,7 +54,7 @@ std::vector<FollowsRelationship*> FollowsExtractor::extractFollows(SimpleToken p
 	
 	for (int i = 0; i < stmtSeries.size(); i++) {
 		SimpleToken current = stmtSeries.at(i);
-		if (current.type == SpTokenType::TWHILE || current.type == SpTokenType::TIF) {
+		if (current.type == SpTokenType::TWHILE || current.type == SpTokenType::TIF || current.type == SpTokenType::TSTMTLIST) {
 			std::vector<FollowsRelationship*> moreFollowsVector = FollowsExtractor::extractFollows(current);
 			followsVector.insert(followsVector.end(), moreFollowsVector.begin(), moreFollowsVector.end());
 		}
@@ -125,7 +125,7 @@ std::vector<FollowsTRelationship*> FollowsExtractor::extractFollowsT(SimpleToken
 
 	for (int i = 0; i < stmtSeries.size(); i++) {
 		SimpleToken current = stmtSeries.at(i);
-		if (current.type == SpTokenType::TWHILE || current.type == SpTokenType::TIF) {
+		if (current.type == SpTokenType::TWHILE || current.type == SpTokenType::TIF || current.type == SpTokenType::TSTMTLIST) {
 			std::vector<FollowsTRelationship*> moreFollowsTVector = FollowsExtractor::extractFollowsT(current);
 			followsTVector.insert(followsTVector.end(), moreFollowsTVector.begin(), moreFollowsTVector.end());
 		}
