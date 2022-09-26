43a60e516020be33efc75ed780a8dc8dc24d3530
Anthony Lie <e0407672@u.nus.edu>
2022-09-26 18:40:28 +0800

    Add unit testing for nextRelationship

--- Team17/Code17/src/unit_testing/src/models/RelationshipTest.cpp
+++ Team17/Code17/src/unit_testing/src/models/RelationshipTest.cpp
@@ -5,6 +5,7 @@
 #include "models/Relationship/ParentTRelationship.h"
 #include "models/Relationship/FollowsRelationship.h"
 #include "models/Relationship/FollowsTRelationship.h"
+#include "models/Relationship/NextRelationship.h"
 
 #include "models/Entity/Entity.h"
 #include "models/Entity/AssignEntity.h"
@@ -364,6 +365,70 @@ TEST_CASE("FollowsT Relationship Test") {
     Relationship* ifCallRel = new FollowsTRelationship(ifEntity, callEntity);
     Relationship* ifWhileRel = new FollowsTRelationship(ifEntity, whileEntity);
     Relationship* ifIfRel = new FollowsTRelationship(ifEntity, ifEntity2);
+
+    REQUIRE(*(ifAssignRel->getLeftEntity()) == *ifEntity);
+    REQUIRE(*(ifReadRel->getLeftEntity()) == *ifEntity);
+    REQUIRE(*(ifPrintRel->getLeftEntity()) == *ifEntity);
+    REQUIRE(*(ifCallRel->getLeftEntity()) == *ifEntity);
+    REQUIRE(*(ifWhileRel->getLeftEntity()) == *ifEntity);
+    REQUIRE(*(ifIfRel->getLeftEntity()) == *ifEntity);
+
+    REQUIRE(*(ifAssignRel->getRightEntity()) == *assignEntity);
+    REQUIRE(*(ifReadRel->getRightEntity()) == *readEntity);
+    REQUIRE(*(ifPrintRel->getRightEntity()) == *printEntity);
+    REQUIRE(*(ifCallRel->getRightEntity()) == *callEntity);
+    REQUIRE(*(ifWhileRel->getRightEntity()) == *whileEntity);
+    REQUIRE(*(ifIfRel->getRightEntity()) == *ifEntity2);
+}
+
+TEST_CASE("Next Relationship Test") {
+    std::string whileValue = "1";
+    std::string ifValue = "2";
+
+    std::string assignValue = "3";
+    std::string readValue = "4";
+    std::string printValue = "5";
+    std::string callValue = "6";
+    std::string whileValue2 = "7";
+    std::string ifValue2 = "8";
+
+    Entity* whileEntity = new WhileEntity(whileValue);
+    Entity* ifEntity = new IfEntity(ifValue);
+
+    Entity* readEntity = new ReadEntity(readValue);
+    Entity* printEntity = new PrintEntity(printValue);
+    Entity* assignEntity = new AssignEntity(ifValue);
+    Entity* callEntity = new CallEntity(callValue);
+    Entity* whileEntity2 = new WhileEntity(whileValue2);
+    Entity* ifEntity2 = new IfEntity(ifValue2);
+
+    Relationship* whileAssignRel = new NextRelationship(whileEntity, assignEntity);
+    Relationship* whileReadRel = new NextRelationship(whileEntity, readEntity);
+    Relationship* whilePrintRel = new NextRelationship(whileEntity, printEntity);
+    Relationship* whileCallRel = new NextRelationship(whileEntity, callEntity);
+    Relationship* whileWhileRel = new NextRelationship(whileEntity, whileEntity2);
+    Relationship* whileIfRel = new NextRelationship(whileEntity, ifEntity);
+
+    REQUIRE(*(whileAssignRel->getLeftEntity()) == *whileEntity);
+    REQUIRE(*(whileReadRel->getLeftEntity()) == *whileEntity);
+    REQUIRE(*(whilePrintRel->getLeftEntity()) == *whileEntity);
+    REQUIRE(*(whileCallRel->getLeftEntity()) == *whileEntity);
+    REQUIRE(*(whileWhileRel->getLeftEntity()) == *whileEntity);
+    REQUIRE(*(whileIfRel->getLeftEntity()) == *whileEntity);
+
+    REQUIRE(*(whileAssignRel->getRightEntity()) == *assignEntity);
+    REQUIRE(*(whileReadRel->getRightEntity()) == *readEntity);
+    REQUIRE(*(whilePrintRel->getRightEntity()) == *printEntity);
+    REQUIRE(*(whileCallRel->getRightEntity()) == *callEntity);
+    REQUIRE(*(whileWhileRel->getRightEntity()) == *whileEntity2);
+    REQUIRE(*(whileIfRel->getRightEntity()) == *ifEntity);
+
+    Relationship* ifAssignRel = new NextRelationship(ifEntity, assignEntity);
+    Relationship* ifReadRel = new NextRelationship(ifEntity, readEntity);
+    Relationship* ifPrintRel = new NextRelationship(ifEntity, printEntity);
+    Relationship* ifCallRel = new NextRelationship(ifEntity, callEntity);
+    Relationship* ifWhileRel = new NextRelationship(ifEntity, whileEntity);
+    Relationship* ifIfRel = new NextRelationship(ifEntity, ifEntity2);
 
     REQUIRE(*(ifAssignRel->getLeftEntity()) == *ifEntity);
     REQUIRE(*(ifReadRel->getLeftEntity()) == *ifEntity);
