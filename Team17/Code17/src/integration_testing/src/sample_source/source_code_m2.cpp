92fd1ba168b79d68655ffa63eafd96b9ccd66f52
Andrea Tan <andreaxtan611@gmail.com>
2022-10-02 22:57:29 +0800

    Add sample source 2 line number

--- Team17/Code17/src/integration_testing/src/sample_source/source_code_m2.cpp
+++ Team17/Code17/src/integration_testing/src/sample_source/source_code_m2.cpp
@@ -2,26 +2,24 @@
 
 std::string sampleSource2 =
         "procedure First {"
-        "      read x;"
-        "      read z;\n"
-        "      call Second; }\n"
-        "\n"
+        "      read x;" //1
+        "      read z;\n" //2
+        "      call Second; }\n" //3
         "procedure Second {\n"
-        "        x = 0;\n"
-        "        i = 5;\n"
-        "        while (i!=0) {\n"
-        "            x = x + 2*y;\n"
-        "            call Third;\n"
-        "            i = i - 1; }\n"
-        "        if (x==1) then {\n"
-        "            x = x+1; }\n"
+        "        x = 0;\n" //4
+        "        i = 5;\n" //5
+        "        while (i!=0) {\n" //6
+        "            x = x + 2*y;\n" //7
+        "            call Third;\n" //8
+        "            i = i - 1; }\n" //9
+        "        if (x==1) then {\n" //10
+        "            x = x+1; }\n" //11
         "        else {\n"
-        "            z = 1; }\n"
-        "        z = z + x + i;\n"
-        "        y = z + 2;\n"
-        "        x = x * y + z; }\n"
-        "\n"
+        "            z = 1; }\n" //12
+        "        z = z + x + i;\n" //13
+        "        y = z + 2;\n" //14
+        "        x = x * y + z; }\n" //15
         "procedure Third {\n"
-        "        z = 5;\n"
-        "        v = z;\n"
-        "        print v; }";
+        "        z = 5;\n" //16
+        "        v = z;\n" //17
+        "        print v; }"; //18
