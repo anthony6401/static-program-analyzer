132a6af0716ee39c7ef58cf2bd9c07c837f9b9dd
Andrea Tan <andreaxtan611@gmail.com>
2022-10-02 23:07:48 +0800

    Add test cases templates

--- Team17/Code17/src/integration_testing/src/QPS-PKB-test-m2.cpp
+++ Team17/Code17/src/integration_testing/src/QPS-PKB-test-m2.cpp
@@ -1,4 +1,22 @@
+#include <catch.hpp>
+#include "components/qps/QPS.h"
+
 // Populating data in PKB
 
 
 // PQL queries
+TEST_CASE("Calls queries") {
+
+}
+
+TEST_CASE("Calls* queries") {
+
+}
+
+TEST_CASE("Next queries") {
+
+}
+
+TEST_CASE("Next* queries") {
+
+}
