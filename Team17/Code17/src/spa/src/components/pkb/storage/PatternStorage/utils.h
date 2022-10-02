b64a74e8b9cf4966a1337343c3d2032f6b0917ae
Kacalamaka <heinrich2001.ang@gmail.com>
2022-10-02 16:52:16 +0800

    clean up code

--- Team17/Code17/src/spa/src/components/pkb/storage/PatternStorage/utils.h
+++ Team17/Code17/src/spa/src/components/pkb/storage/PatternStorage/utils.h
@@ -17,38 +17,6 @@ struct pair_hash {
 
 class PatternUtils {
 public:
-    /*static bool isSubExpression(const std::string& sub_expression, const std::string& full_expression) {
-        size_t idx = full_expression.find_first_of(" *+/-%()");
-        size_t prev = 0;
-
-        if (idx == std::string::npos) {
-            return sub_expression == full_expression;
-        }
-
-        while (idx != std::string::npos) {
-            std::string str;
-            if (prev == 0) {
-                str = full_expression.substr(prev, idx - prev);
-            }
-            else {
-                str = full_expression.substr(prev + 1, idx - prev - 1);
-            }
-
-            if (str.size() > 0 && str.find_first_of(" *+/-%()") == std::string::npos && str == sub_expression) {
-                return true;
-                
-            }
-
-            prev = idx;
-            idx = full_expression.find_first_of(" *+/-%()", idx + 1);
-        }
-        std::string lastString = full_expression.substr(prev + 1, full_expression.size() - prev);
-        if (lastString.size() > 0 && lastString.find_first_of(" *+/-()") == std::string::npos && lastString == sub_expression) {
-            return true;
-        }
-
-        return false;
-    }*/
     // Helper function
     static bool isAlphanumeric(char c) {
         return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
@@ -120,19 +88,6 @@ public:
         std::istream_iterator<std::string> fullExpBegin(fullExpStream);
         std::istream_iterator<std::string> fullExpEnd;
         std::vector<std::string> fullExpressionArr(fullExpBegin, fullExpEnd);
-
-        //Debugging
-
-      /*  for (auto s : subExpressionArr) {
-            std::cout << s << std::endl;
-        }
-
-        std::cout << "---------" << std::endl;
-
-        for (auto s : fullExpressionArr) {
-            std::cout << s << std::endl;
-        }
-        std::cout << "End of debug" << std::endl;*/
 
         return isSubArray(fullExpressionArr, subExpressionArr);
     }
