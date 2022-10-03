20179d8fc05c4bd4a15b1254d4643b5b68e7b399
Andrea Tan <andreaxtan611@gmail.com>
2022-10-03 09:52:04 +0800

    Rename pkb

--- Team17/Code17/src/integration_testing/src/QPS-PKB-test-m2.cpp
+++ Team17/Code17/src/integration_testing/src/QPS-PKB-test-m2.cpp
@@ -1,13 +1,20 @@
 #include <catch.hpp>
 #include "components/qps/QPS.h"
+#include "components/qps/abstract_query_object/QueryObject.h"
+#include "components/qps/query_evaluator/Evaluator.h"
 
 // Populating data in PKB
-
+PKB* pkb_m2 = new PKB();
+auto qpsClient_m2 = QPSClient(pkb_m2);
 
 // PQL queries
 TEST_CASE("Calls queries") {
-    SECTION("") {
-
+    SECTION("Calls") {
+        std::string testQuery = "if ifs; Select ifs";
+        std::list<std::string> testResults;
+        std::list<std::string> expectedResults = {"8"};
+        QPS::processQueryResult(testQuery, testResults, qpsClient_m2);
+        REQUIRE(testResults == expectedResults);
     }
 }
 
