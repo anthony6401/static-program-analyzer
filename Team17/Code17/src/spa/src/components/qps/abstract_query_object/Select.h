dfaa9bca2badeb85faa918f883e8a1941a08caa5
Andrea Tan <andreaxtan611@gmail.com>
2022-10-03 21:40:13 +0800

    Update Select class

--- Team17/Code17/src/spa/src/components/qps/abstract_query_object/Select.h
+++ Team17/Code17/src/spa/src/components/qps/abstract_query_object/Select.h
@@ -3,19 +3,29 @@
 
 #include "components/qps/query_preprocessor/query_tokenizer/TokenType.h"
 #include "components/qps/query_preprocessor/query_tokenizer/TokenObject.h"
+#include <vector>
 
 using namespace qps;
 
 class Select {
 private:
+    TokenType returnType;
+    std::vector<TokenObject> returnValues;
+    //To remove
     std::string synonym;
 
 public:
     Select();
     Select(std::string synonym);
+    Select(TokenType returnType);
+    Select(TokenType returnType, std::vector<TokenObject> returnValues);
     std::string getSynonym();
+    TokenType getReturnType();
+    std::vector<TokenObject> getReturnValues();
     bool operator==(const Select& other) const {
-        return synonym == other.synonym;
+        return synonym == other.synonym
+        && returnValues == other.returnValues &&
+        returnType == other.returnType;
     }
 };
 
