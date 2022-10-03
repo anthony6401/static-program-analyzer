123e90a3849258c002f1fa2454ce3c03a303e022
Andrea Tan <andreaxtan611@gmail.com>
2022-10-03 09:59:28 +0800

    Remove additional files

--- Team17/Code17/src/spa/src/models/Relationship/CallsTRelationship 2.cpp
+++
@@ -1,6 +0,0 @@
-#include "CallsTRelationship.h"
-#include "Relationship.h"
-
-#include "models/Entity/Entity.h"
-
-CallsTRelationship::CallsTRelationship(Entity* leftEntity, Entity* rightEntity) : Relationship(leftEntity, rightEntity) {}
--- Team17/Code17/src/spa/src/models/Relationship/CallsTRelationship 2.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Relationship.h"
-#include "models/Entity/Entity.h"
-
-class CallsTRelationship : public Relationship {
-public:
-	CallsTRelationship(Entity* leftEntity, Entity* rightEntity);
-};
