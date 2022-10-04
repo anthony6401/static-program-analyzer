8a3010271807925e47e3c3e9f0d5cf3a4f19aa1d
hyngkng <hyngkng@gmail.com>
2022-10-04 14:30:21 +0800

    Fix build issue

--- Team17/Code17/src/spa/src/components/sp/validator/SimpleValidator.h
+++ Team17/Code17/src/spa/src/components/sp/validator/SimpleValidator.h
@@ -24,7 +24,7 @@ class SimpleValidator {
         static bool isAndOrCenter(std::vector<std::string> tokens, int connectorPosition);
         static bool isValidCalls(std::multimap<std::string, std::string> &callProcedures,
             std::set<std::string>& procedures);
-        static bool SimpleValidator::isCyclic(std::multimap<std::string, std::string>& callProcedures,
+        static bool isCyclic(std::multimap<std::string, std::string>& callProcedures,
             std::map<std::string, bool> visited, std::string procedure);
 
 };
