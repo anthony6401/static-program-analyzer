feac41e2c8bff34348e22ebc91fdaaa4a6433fe4
hyngkng <hyngkng@gmail.com>
2022-10-04 15:28:53 +0800

    uncomment test cases

--- Team17/Code17/src/integration_testing/src/SP-PKB-test-m2.cpp
+++ Team17/Code17/src/integration_testing/src/SP-PKB-test-m2.cpp
@@ -44,69 +44,69 @@ TEST_CASE("test SP PKB integration m2") {
 // pkbSP APIs queries
 // Uncomment all test cases when SP has populate PKB
 TEST_CASE("Select all queries for SP m2") {
-    //SECTION("Select call statements") {
-    //    std::unordered_set<std::string> testResults = pkbSP_m2->getAllEntity(DesignEntity::CALL);
-    //    std::unordered_set<std::string> expectedResults = { "Third", "Second", "First" };
-    //    REQUIRE(testResults == expectedResults);
-    //}
+    SECTION("Select call statements") {
+        std::unordered_set<std::string> testResults = pkbSP_m2->getAllEntity(DesignEntity::CALL);
+        std::unordered_set<std::string> expectedResults = { "Third", "Second", "First" };
+        REQUIRE(testResults == expectedResults);
+    }
 }
 
 TEST_CASE("Calls queries SP") {
-    //SECTION("Calls with no synonym") {
-    //    // Calls("First", "Second")
-    //    bool testResults = pkbSP_m2->getRelationship(RelationshipType::CALLS, TokenObject(TokenType::NAME_WITH_QUOTATION, "First"), TokenObject(TokenType::NAME_WITH_QUOTATION, "Second"));
-    //    REQUIRE(testResults);
-    //}
+    SECTION("Calls with no synonym") {
+        // Calls("First", "Second")
+        bool testResults = pkbSP_m2->getRelationship(RelationshipType::CALLS, TokenObject(TokenType::NAME_WITH_QUOTATION, "First"), TokenObject(TokenType::NAME_WITH_QUOTATION, "Second"));
+        REQUIRE(testResults);
+    }
 
-    //SECTION("Calls with synonym first argument") {
-    //    // Calls(p, "Third")
-    //    std::unordered_set<std::string> testResults = pkbSP_m2->getRelationshipBySecond(RelationshipType::CALLS, DesignEntity::PROCEDURE, TokenObject(TokenType::NAME_WITH_QUOTATION, "Third"));
-    //    std::unordered_set<std::string> expectedResults = { "Second" };
-    //    REQUIRE(testResults == expectedResults);
-    //}
+    SECTION("Calls with synonym first argument") {
+        // Calls(p, "Third")
+        std::unordered_set<std::string> testResults = pkbSP_m2->getRelationshipBySecond(RelationshipType::CALLS, DesignEntity::PROCEDURE, TokenObject(TokenType::NAME_WITH_QUOTATION, "Third"));
+        std::unordered_set<std::string> expectedResults = { "Second" };
+        REQUIRE(testResults == expectedResults);
+    }
 
-    //SECTION("Calls with synonym second argument") {
-    //    // Calls("First", p)
-    //    std::unordered_set<std::string> testResults = pkbSP_m2->getRelationshipByFirst(RelationshipType::CALLS, TokenObject(TokenType::NAME_WITH_QUOTATION, "First"), DesignEntity::PROCEDURE);
-    //    std::unordered_set<std::string> expectedResults = { "Second" };
-    //    REQUIRE(testResults == expectedResults);
-    //}
+    SECTION("Calls with synonym second argument") {
+        // Calls("First", p)
+        std::unordered_set<std::string> testResults = pkbSP_m2->getRelationshipByFirst(RelationshipType::CALLS, TokenObject(TokenType::NAME_WITH_QUOTATION, "First"), DesignEntity::PROCEDURE);
+        std::unordered_set<std::string> expectedResults = { "Second" };
+        REQUIRE(testResults == expectedResults);
+    }
 
-    //SECTION("Calls with 2 synonyms") {
-    //    // Calls(p1, p2)
-    //    std::unordered_map<std::string, std::unordered_set<std::string>> testResults = pkbSP_m2->getAllRelationship(RelationshipType::CALLS, DesignEntity::PROCEDURE, DesignEntity::PROCEDURE);
-    //    std::unordered_map<std::string, std::unordered_set<std::string>> expectedResults = { {"First", std::unordered_set<std::string>({"Second"})},
-    //                                                                                            {"Second", std::unordered_set<std::string>({"Third"})} };
-    //    REQUIRE(testResults == expectedResults);
-    //}
+    SECTION("Calls with 2 synonyms") {
+        // Calls(p1, p2)
+        std::unordered_map<std::string, std::unordered_set<std::string>> testResults = pkbSP_m2->getAllRelationship(RelationshipType::CALLS, DesignEntity::PROCEDURE, DesignEntity::PROCEDURE);
+        std::unordered_map<std::string, std::unordered_set<std::string>> expectedResults = { {"First", std::unordered_set<std::string>({"Second"})},
+                                                                                                {"Second", std::unordered_set<std::string>({"Third"})} };
+        REQUIRE(testResults == expectedResults);
+    }
 }
 
 TEST_CASE("Calls* queries SP") {
-    //SECTION("Calls* with no synonym") {
-    //    // Calls*("First", "Second")
-    //    bool testResults = pkbSP_m2->getRelationship(RelationshipType::CALLS_T, TokenObject(TokenType::NAME_WITH_QUOTATION, "First"), TokenObject(TokenType::NAME_WITH_QUOTATION, "Second"));
-    //    REQUIRE(testResults);
-    //}
+    SECTION("Calls* with no synonym") {
+        // Calls*("First", "Second")
+        bool testResults = pkbSP_m2->getRelationship(RelationshipType::CALLS_T, TokenObject(TokenType::NAME_WITH_QUOTATION, "First"), TokenObject(TokenType::NAME_WITH_QUOTATION, "Second"));
+        REQUIRE(testResults);
+    }
 
-    //SECTION("Calls* with synonym first argument") {
-    //    // Calls*(p, "Third")
-    //    std::unordered_set<std::string> testResults = pkbSP_m2->getRelationshipBySecond(RelationshipType::CALLS_T, DesignEntity::PROCEDURE, TokenObject(TokenType::NAME_WITH_QUOTATION, "Third"));
-    //    std::unordered_set<std::string> expectedResults = { "First", "Second" };
-    //    REQUIRE(testResults == expectedResults);
-    //}
+    SECTION("Calls* with synonym first argument") {
+        // Calls*(p, "Third")
+        std::unordered_set<std::string> testResults = pkbSP_m2->getRelationshipBySecond(RelationshipType::CALLS_T, DesignEntity::PROCEDURE, TokenObject(TokenType::NAME_WITH_QUOTATION, "Third"));
+        std::unordered_set<std::string> expectedResults = { "First", "Second" };
+        REQUIRE(testResults == expectedResults);
+    }
 
-    //SECTION("Calls* with synonym second argument") {
-    //    // Calls*("First", p)
-    //    std::unordered_set<std::string> testResults = pkbSP_m2->getRelationshipByFirst(RelationshipType::CALLS_T, TokenObject(TokenType::NAME_WITH_QUOTATION, "First"), DesignEntity::PROCEDURE);
-    //    std::unordered_set<std::string> expectedResults = { "Second", "Third" };
-    //    REQUIRE(testResults == expectedResults);
-    //}
+    SECTION("Calls* with synonym second argument") {
+        // Calls*("First", p)
+        std::unordered_set<std::string> testResults = pkbSP_m2->getRelationshipByFirst(RelationshipType::CALLS_T, TokenObject(TokenType::NAME_WITH_QUOTATION, "First"), DesignEntity::PROCEDURE);
+        std::unordered_set<std::string> expectedResults = { "Second", "Third" };
+        REQUIRE(testResults == expectedResults);
+    }
 
-    //SECTION("Calls* with 2 synonyms") {
-    //    // Calls*(p1, p2)
-    //    std::unordered_map<std::string, std::unordered_set<std::string>> testResults = pkbSP_m2->getAllRelationship(RelationshipType::CALLS_T, DesignEntity::PROCEDURE, DesignEntity::PROCEDURE);
-    //    std::unordered_map<std::string, std::unordered_set<std::string>> expectedResults = { {"First", std::unordered_set<std::string>({"Second", "Third" })},
-    //                                                                                            {"Second", std::unordered_set<std::string>({"Third"})} };
-    //    REQUIRE(testResults == expectedResults);
-    //}
+    SECTION("Calls* with 2 synonyms") {
+        // Calls*(p1, p2)
+        std::unordered_map<std::string, std::unordered_set<std::string>> testResults = pkbSP_m2->getAllRelationship(RelationshipType::CALLS_T, DesignEntity::PROCEDURE, DesignEntity::PROCEDURE);
+        std::unordered_map<std::string, std::unordered_set<std::string>> expectedResults = { {"First", std::unordered_set<std::string>({"Second", "Third" })},
+                                                                                                {"Second", std::unordered_set<std::string>({"Third"})} };
+        REQUIRE(testResults == expectedResults);
+    }
 }
