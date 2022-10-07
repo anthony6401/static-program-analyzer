ee03721545f5f1ea1f0b70c3a6bb279dcc000722
Andrea Tan <andreaxtan611@gmail.com>
2022-10-07 11:14:25 +0800

    Add Select attribute clause

--- Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/select/SelectAttributeClause.cpp
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/select/SelectAttributeClause.cpp
@@ -1,5 +1,22 @@
-//
-// Created by ANDREA TAN on 7/10/22.
-//
-
 #include "SelectAttributeClause.h"
+#include "SelectClause.h"
+#include "components/pkb/pkb.h"
+#include "components/pkb/clients/QPSClient.h"
+#include "iostream"
+
+SelectAttributeClause::SelectAttributeClause(TokenObject selectSynonym, std::string attributeName, std::unordered_set<std::string> &synonymsInTable, std::unordered_map<std::string, DesignEntity> synonymToDesignEntityMap, QPSClient qpsClient)
+        : selectSynonym(selectSynonym), attributeName(attributeName), synonymsInTable(synonymsInTable), synonymToDesignEntityMap(synonymToDesignEntityMap), qpsClient(qpsClient) {}
+
+ResultTable SelectAttributeClause::evaluateClause() {
+
+}
+
+size_t SelectAttributeClause::getNumberOfSynonyms() {
+    return 1;
+}
+
+std::set<std::string> SelectAttributeClause::getAllSynonyms() {
+    std::set<std::string> synonyms = {};
+    synonyms.emplace(selectSynonym.getValue());
+    return synonyms;
+}
--- Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/select/SelectAttributeClause.h
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/select/SelectAttributeClause.h
@@ -1,8 +1,26 @@
-//
-// Created by ANDREA TAN on 7/10/22.
-//
-
 #ifndef SPA_SELECTATTRIBUTECLAUSE_H
 #define SPA_SELECTATTRIBUTECLAUSE_H
+
+#include <unordered_map>
+#include <list>
+#include "components/qps/query_evaluator/factory/interface/Clause.h"
+#include "components/qps/abstract_query_object/Select.h"
+#include "components/pkb/clients/QPSClient.h"
+#include "models/Entity/DesignEntity.h"
+
+class SelectAttributeClause : public Clause {
+private:
+    std::unordered_map<std::string, DesignEntity> synonymToDesignEntityMap;
+    std::unordered_set<std::string> synonymsInTable;
+    TokenObject selectSynonym;
+    std::string attributeName;
+    QPSClient qpsClient;
+
+public:
+    SelectAttributeClause(TokenObject selectSynonym, std::string attributeName, std::unordered_set<std::string> &synonymsInTable, std::unordered_map<std::string, DesignEntity> synonymToDesignEntityMap, QPSClient qpsClient);
+    ResultTable evaluateClause() override;
+    size_t getNumberOfSynonyms() override;
+    std::set<std::string> getAllSynonyms() override;
+};
 
 #endif //SPA_SELECTATTRIBUTECLAUSE_H
