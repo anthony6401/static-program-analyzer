655eb0085f3dc2d2be3265d63b0127825f5191fc
Kacalamaka <heinrich2001.ang@gmail.com>
2022-10-08 16:19:47 +0800

    Created new If Pattern

---
+++ Team17/Code17/src/spa/src/models/Pattern/IfPattern.cpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "IfPattern.h"
+#include <string>
+
+IfPattern::IfPattern(std::string lineNum, std::string firstValue, std::string secondValue) : Pattern(lineNum, firstValue, secondValue) {}
