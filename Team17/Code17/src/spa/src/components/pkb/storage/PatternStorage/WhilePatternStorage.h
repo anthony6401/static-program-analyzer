7dae73d1d777416906d3596cb3531e04c9c34216
Kacalamaka <heinrich2001.ang@gmail.com>
2022-10-08 18:10:30 +0800

    Created While Pattern Storage

---
+++ Team17/Code17/src/spa/src/components/pkb/storage/PatternStorage/WhilePatternStorage.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include "PatternStorage.h"
+#include "utils.h"
+#include "components/qps/query_preprocessor/query_tokenizer/TokenObject.h"
+
+class WilePatternStorage : public PatternStorage {
+
+public:
+	IfPatternStorage();
+	bool storePattern(kb::Pattern* pattern);
+	std::unordered_set<std::string> getPattern(DesignEntity designEntity, TokenObject firstArgument, TokenObject secondArgument);
+	std::vector<std::pair<std::string, std::string>> getPatternPair(DesignEntity designEntity, TokenObject secondArgument);
+
+private:
+	// Note that the storage takes in the lineNum of the Pattern as the key and firstValue as value.
+	std::unordered_map<std::string, std::unordered_set<std::string>> whilePatternStorage;
+};
