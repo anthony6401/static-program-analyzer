908a3eff8ed8379f3fcb5d564dd33da29e374231
Andrea Tan <andreaxtan611@gmail.com>
2022-10-09 21:04:05 +0800

    Add if pattern clause template

--- Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/patterns/IfPatternClause.cpp
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/patterns/IfPatternClause.cpp
@@ -1,5 +1,35 @@
-//
-// Created by ANDREA TAN on 9/10/22.
-//
-
 #include "IfPatternClause.h"
+
+IfPatternClause::IfPatternClause(std::string ifSynonym, TokenObject firstArgument, QPSClient qpsClient)
+        : ifSynonym(ifSynonym), firstArgument(firstArgument), qpsClient(qpsClient) {}
+
+ResultTable IfPatternClause::evaluateClause() {
+    TokenType firstArgumentType = firstArgument.getTokenType();
+    if (firstArgumentType == TokenType::NAME || firstArgumentType == TokenType::WILDCARD) {
+
+    } else {
+
+    }
+
+}
+
+size_t IfPatternClause::getNumberOfSynonyms() {
+
+}
+std::set<std::string> IfPatternClause::getAllSynonyms() {
+
+}
+
+ResultTable IfPatternClause::evaluateFirstArgAsSynonym() {
+
+}
+
+ResultTable IfPatternClause::evaluateFirstArgAsNameQuotes() {
+
+}
+
+
+ResultTable IfPatternClause::evaluateFirstArgAsWildcard() {
+
+}
+
