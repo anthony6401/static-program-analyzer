ea4ddbbb75e4fc5f3f1965bfaec43f3c22650598
Andrea Tan <andreaxtan611@gmail.com>
2022-10-10 12:57:06 +0800

    Add with clause template

---
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/with/WithClause.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ANDREA TAN on 10/10/22.
+//
+
+#include "WithClause.h"
---
+++ Team17/Code17/src/spa/src/components/qps/query_evaluator/factory/clauses/with/WithClause.h
@@ -0,0 +1,36 @@
+#ifndef SPA_WITHCLAUSE_H
+#define SPA_WITHCLAUSE_H
+
+#include <unordered_map>
+#include <list>
+#include "components/qps/query_evaluator/factory/interface/Clause.h"
+#include "components/qps/abstract_query_object/Select.h"
+#include "components/pkb/clients/QPSClient.h"
+#include "models/Entity/DesignEntity.h"
+
+class WithClause : public Clause {
+private:
+    TokenType leftType;
+    TokenType rightType;
+    std::vector<TokenObject> left;
+    std::vector<TokenObject> right;
+    std::unordered_map<std::string, DesignEntity> synonymToDesignEntityMap;
+    QPSClient qpsClient;
+
+public:
+    WithClause(TokenType leftType, TokenType rightType, std::vector<TokenObject> left, std::vector<TokenObject> right, std::unordered_map<std::string, DesignEntity> synonymToDesignEntityMap, QPSClient qpsClient);
+    ResultTable evaluateClause() override;
+    size_t getNumberOfSynonyms() override;
+    std::set<std::string> getAllSynonyms() override;
+    ResultTable evaluateAttributeAttribute();
+    ResultTable evaluateAttributeInteger();
+    ResultTable evaluateAttributeNameQuotes();
+    ResultTable evaluateIntegerAttribute();
+    ResultTable evaluateIntegerInteger();
+    ResultTable evaluateIntegerNameQuotes();
+    ResultTable evaluateNameQuotesAttribute();
+    ResultTable evaluateNameQuotesInteger();
+    ResultTable evaluateNameQuotesNameQuotes();
+};
+
+#endif //SPA_WITHCLAUSE_H
