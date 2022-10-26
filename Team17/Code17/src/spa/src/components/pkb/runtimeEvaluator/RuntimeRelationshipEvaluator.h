44f7f9b1c041b77acd1850f44749f2a7e79fcf2a
Kacalamaka <heinrich2001.ang@gmail.com>
2022-10-26 11:35:50 +0800

    Revert "Added caching for optimization"
    This reverts commit 5c864a8660b17efded485ee5041ef1ea566327a9.

--- Team17/Code17/src/spa/src/components/pkb/runtimeEvaluator/RuntimeRelationshipEvaluator.h
+++ Team17/Code17/src/spa/src/components/pkb/runtimeEvaluator/RuntimeRelationshipEvaluator.h
@@ -14,16 +14,6 @@ public:
 	virtual bool getRuntimeRelationship(RelationshipType relType, TokenObject firstArgument, TokenObject secondArgument) = 0;
 	virtual std::unordered_set<std::string> getRuntimeRelationshipByFirst(RelationshipType relType, TokenObject firstArgument, std::unordered_set<std::string>& filter) = 0;
 	virtual std::unordered_set<std::string> getRuntimeRelationshipBySecond(RelationshipType relType, TokenObject secondArgument, std::unordered_set<std::string>& filter) = 0;
-	void clearCache();
 	virtual std::unordered_map<std::string, std::unordered_set<std::string>> getAllRuntimeRelationship(RelationshipType relType, std::unordered_set<std::string>& filter1, std::unordered_set<std::string>& filter2) = 0;
 	~RuntimeRelationshipEvaluator() = default;
-
-protected:
-	bool getForwardCacheKV(std::string key, std::string value);
-	std::unordered_set<std::string> getForwardCache(std::string key);
-	std::unordered_set<std::string> getBackwardCache(std::string key);
-	void storeForwardCache(std::string key, std::string value);
-	void storeBackwardCache(std::string key, std::string value);
-	std::unordered_map<std::string, std::unordered_set<std::string>> forwardCache;
-	std::unordered_map<std::string, std::unordered_set<std::string>> backwardCache;
 };
