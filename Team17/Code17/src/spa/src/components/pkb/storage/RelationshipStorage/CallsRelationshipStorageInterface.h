719ba8f762e21b2e77630ea2590c68735907d2d3
Anthony Lie <e0407672@u.nus.edu>
2022-10-26 18:23:40 +0800

    Add API for Calls storages

--- Team17/Code17/src/spa/src/components/pkb/storage/RelationshipStorage/CallsRelationshipStorageInterface.h
+++ Team17/Code17/src/spa/src/components/pkb/storage/RelationshipStorage/CallsRelationshipStorageInterface.h
@@ -9,9 +9,15 @@ public:
 	bool getRelationship(RelationshipType relType, TokenObject firstArgument, TokenObject secondArgument);
 	std::unordered_set<std::string> getRelationshipByFirst(RelationshipType relType, TokenObject firstArgument, DesignEntity returnType);
 	std::unordered_set<std::string> getRelationshipBySecond(RelationshipType relType, DesignEntity returnType, TokenObject secondArgument);
+	std::unordered_set<std::string> getRelationshipWithFirstWilcard(RelationshipType relType, DesignEntity returnType);
+	std::unordered_set<std::string> getRelationshipWithSecondWildcard(RelationshipType relType, DesignEntity returnType);
 	std::unordered_map<std::string, std::unordered_set<std::string>> getAllRelationship(RelationshipType relType, DesignEntity returnType1, DesignEntity returnType2);
 
 protected:
+	bool handleConstantConstant(TokenObject firstArgument, TokenObject secondArgument);
+	bool handleConstantWildcard(TokenObject firstArgument);
+	bool handleWildcardConstant(TokenObject secondArgument);
+	bool handleWilcardWildcard();
 	RelationshipType relationshipType;
 
 	// To answer Calls("proc1", "proc2"), Calls("proc1", p), Calls(p1, p2)
