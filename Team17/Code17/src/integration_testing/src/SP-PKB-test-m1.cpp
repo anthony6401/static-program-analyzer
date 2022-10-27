0269a74f2320f26b276a8f7be138c7dadaf32e0d
Anthony Lie <e0407672@u.nus.edu>
2022-10-27 18:11:48 +0800

    Add wildcard API integration test for SP-PKB

--- Team17/Code17/src/integration_testing/src/SP-PKB-test-m1.cpp
+++ Team17/Code17/src/integration_testing/src/SP-PKB-test-m1.cpp
@@ -101,10 +101,16 @@ TEST_CASE("Select all queries for SP") {
     }
 }
 
-TEST_CASE("Relationships and patterns for SP") {
+TEST_CASE("Modifies Relationship for SP") {
     SECTION("Modifies with no synonym") {
         // Modifies(2, cenX)
         bool testResults = pkbSP->getRelationship(RelationshipType::MODIFIES, TokenObject(TokenType::INTEGER, "2"), TokenObject(TokenType::NAME_WITH_QUOTATION, "cenX"));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Modifies with value and wildcard") {
+        // Modifies(3, _)
+        bool testResults = pkbSP->getRelationship(RelationshipType::MODIFIES, TokenObject(TokenType::INTEGER, "3"), TokenObject(TokenType::WILDCARD, ""));
         REQUIRE(testResults);
     }
 
@@ -119,6 +125,14 @@ TEST_CASE("Relationships and patterns for SP") {
         // Modifies(1, v)
         std::unordered_set<std::string> testResults = pkbSP->getRelationshipByFirst(RelationshipType::MODIFIES, TokenObject(TokenType::INTEGER, "1"), DesignEntity::VARIABLE);
         std::unordered_set<std::string> expectedResults = { "count" };
+        REQUIRE(testResults == expectedResults);
+    }
+
+
+    SECTION("Modifies with synonym and wilcard") {
+        // Modifies(a, _)
+        std::unordered_set<std::string> testResults = pkbSP->getRelationshipWithSecondWildcard(RelationshipType::MODIFIES, DesignEntity::ASSIGN);
+        std::unordered_set<std::string> expectedResults = { "1", "2", "3", "5", "6", "7", "9", "10", "11", "12"};
         REQUIRE(testResults == expectedResults);
     }
 
@@ -137,10 +151,18 @@ TEST_CASE("Relationships and patterns for SP") {
                                                                                                 {"12", std::unordered_set<std::string>({"normSq"})} };
         REQUIRE(testResults == expectedResults);
     }
+}
 
+TEST_CASE("Uses Relationship for SP") {
     SECTION("Uses with no synonym") {
         // Uses(4, "x")
         bool testResults = pkbSP->getRelationship(RelationshipType::USES, TokenObject(TokenType::INTEGER, "4"), TokenObject(TokenType::NAME_WITH_QUOTATION, "x"));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Uses with value and wildcard") {
+        // Uses(7, _)
+        bool testResults = pkbSP->getRelationship(RelationshipType::USES, TokenObject(TokenType::INTEGER, "7"), TokenObject(TokenType::WILDCARD, ""));
         REQUIRE(testResults);
     }
 
@@ -158,6 +180,13 @@ TEST_CASE("Relationships and patterns for SP") {
         REQUIRE(testResults == expectedResults);
     }
 
+    SECTION("Uses with synonym and wilcard") {
+        // Uses(a, _)
+        std::unordered_set<std::string> testResults = pkbSP->getRelationshipWithSecondWildcard(RelationshipType::USES, DesignEntity::ASSIGN);
+        std::unordered_set<std::string> expectedResults = { "5", "6", "7", "10", "11", "12" };
+        REQUIRE(testResults == expectedResults);
+    }
+
     SECTION("Uses with 2 synonyms") {
         // Uses(a, v)
         std::unordered_map<std::string, std::unordered_set<std::string>> testResults = pkbSP->getAllRelationship(RelationshipType::USES, DesignEntity::ASSIGN, DesignEntity::VARIABLE);
@@ -169,10 +198,30 @@ TEST_CASE("Relationships and patterns for SP") {
                                                                                                 {"12", std::unordered_set<std::string>({"cenX", "cenY"})} };
         REQUIRE(testResults == expectedResults);
     }
+}
 
+TEST_CASE("Follows Relationship for SP") {
     SECTION("Follows with no synonym") {
         // Follows(1, 2)
         bool testResults = pkbSP->getRelationship(RelationshipType::FOLLOWS, TokenObject(TokenType::INTEGER, "1"), TokenObject(TokenType::INTEGER, "2"));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Follows with value and wildcard") {
+        // Follows(2, _)
+        bool testResults = pkbSP->getRelationship(RelationshipType::FOLLOWS, TokenObject(TokenType::INTEGER, "2"), TokenObject(TokenType::WILDCARD, ""));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Follows with wildcard and value") {
+        // Follows(_, 11)
+        bool testResults = pkbSP->getRelationship(RelationshipType::FOLLOWS, TokenObject(TokenType::WILDCARD, ""), TokenObject(TokenType::INTEGER, "11"));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Follows with wildcard and wildcard") {
+        // Follows(_, _)
+        bool testResults = pkbSP->getRelationship(RelationshipType::FOLLOWS, TokenObject(TokenType::WILDCARD, ""), TokenObject(TokenType::WILDCARD, ""));
         REQUIRE(testResults);
     }
 
@@ -190,6 +239,20 @@ TEST_CASE("Relationships and patterns for SP") {
         REQUIRE(testResults == expectedResults);
     }
 
+    SECTION("Follows with wildcard and synonym") {
+        // Follows(a, _)
+        std::unordered_set<std::string> testResults = pkbSP->getRelationshipWithSecondWildcard(RelationshipType::FOLLOWS, DesignEntity::ASSIGN);
+        std::unordered_set<std::string> expectedResults = { "1", "2", "3", "5", "6", "10" };
+        REQUIRE(testResults == expectedResults);
+    }
+
+    SECTION("Follows with synonym and wildcard") {
+        // Follows(_, s)
+        std::unordered_set<std::string> testResults = pkbSP->getRelationshipWithFirstWildcard(RelationshipType::FOLLOWS, DesignEntity::STMT);
+        std::unordered_set<std::string> expectedResults = { "2", "3", "4", "6", "7", "8", "11", "12"};
+        REQUIRE(testResults == expectedResults);
+    }
+
     SECTION("Follows with 2 synonyms") {
         // Follows(a, s)
         std::unordered_map<std::string, std::unordered_set<std::string>> testResults = pkbSP->getAllRelationship(RelationshipType::FOLLOWS, DesignEntity::ASSIGN, DesignEntity::STMT);
@@ -201,10 +264,30 @@ TEST_CASE("Relationships and patterns for SP") {
                                                                                                 {"10", std::unordered_set<std::string>({"11"})} };
         REQUIRE(testResults == expectedResults);
     }
+}
 
+TEST_CASE("Follows* Relationship for SP") {
     SECTION("Follows* with no synonym") {
         // Follows*(1, 3)
         bool testResults = pkbSP->getRelationship(RelationshipType::FOLLOWS_T, TokenObject(TokenType::INTEGER, "1"), TokenObject(TokenType::INTEGER, "3"));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Follows* with value and wildcard") {
+        // Follows*(6, _)
+        bool testResults = pkbSP->getRelationship(RelationshipType::FOLLOWS_T, TokenObject(TokenType::INTEGER, "6"), TokenObject(TokenType::WILDCARD, ""));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Follows* with wildcard and value") {
+        // Follows*(_, 12)
+        bool testResults = pkbSP->getRelationship(RelationshipType::FOLLOWS_T, TokenObject(TokenType::WILDCARD, ""), TokenObject(TokenType::INTEGER, "12"));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Follows* with wildcard and wildcard") {
+        // Follows*(_, _)
+        bool testResults = pkbSP->getRelationship(RelationshipType::FOLLOWS_T, TokenObject(TokenType::WILDCARD, ""), TokenObject(TokenType::WILDCARD, ""));
         REQUIRE(testResults);
     }
 
@@ -222,6 +305,20 @@ TEST_CASE("Relationships and patterns for SP") {
         REQUIRE(testResults == expectedResults);
     }
 
+    SECTION("Follows* with wildcard and synonym") {
+        // Follows*(w, _)
+        std::unordered_set<std::string> testResults = pkbSP->getRelationshipWithSecondWildcard(RelationshipType::FOLLOWS, DesignEntity::WHILE);
+        std::unordered_set<std::string> expectedResults = { "4" };
+        REQUIRE(testResults == expectedResults);
+    }
+
+    SECTION("Follows* with synonym and wildcard") {
+        // Follows*(_, if)
+        std::unordered_set<std::string> testResults = pkbSP->getRelationshipWithFirstWildcard(RelationshipType::FOLLOWS, DesignEntity::IF);
+        std::unordered_set<std::string> expectedResults = { "8" };
+        REQUIRE(testResults == expectedResults);
+    }
+
     SECTION("Follows* with 2 synonyms") {
         // Follows*(a, s)
         std::unordered_map<std::string, std::unordered_set<std::string>> testResults = pkbSP->getAllRelationship(RelationshipType::FOLLOWS_T, DesignEntity::ASSIGN, DesignEntity::STMT);
@@ -233,10 +330,30 @@ TEST_CASE("Relationships and patterns for SP") {
                                                                                                 {"10", std::unordered_set<std::string>({"11"})} };
         REQUIRE(testResults == expectedResults);
     }
+}
 
+TEST_CASE("Parent Relationship for SP") {
     SECTION("Parent with no synonym") {
         // Parent(4, 5)
         bool testResults = pkbSP->getRelationship(RelationshipType::PARENT, TokenObject(TokenType::INTEGER, "4"), TokenObject(TokenType::INTEGER, "5"));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Parent with value and wildcard") {
+        // Parent(8, _)
+        bool testResults = pkbSP->getRelationship(RelationshipType::PARENT, TokenObject(TokenType::INTEGER, "8"), TokenObject(TokenType::WILDCARD, ""));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Parent with wildcard and value") {
+        // Parent(_, 9)
+        bool testResults = pkbSP->getRelationship(RelationshipType::PARENT, TokenObject(TokenType::WILDCARD, ""), TokenObject(TokenType::INTEGER, "9"));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Parent with wildcard and wildcard") {
+        // Parent(_, _)
+        bool testResults = pkbSP->getRelationship(RelationshipType::PARENT, TokenObject(TokenType::WILDCARD, ""), TokenObject(TokenType::WILDCARD, ""));
         REQUIRE(testResults);
     }
 
@@ -254,16 +371,50 @@ TEST_CASE("Relationships and patterns for SP") {
         REQUIRE(testResults == expectedResults);
     }
 
+    SECTION("Parent with wildcard and synonym") {
+        // Parent(s, _)
+        std::unordered_set<std::string> testResults = pkbSP->getRelationshipWithSecondWildcard(RelationshipType::PARENT, DesignEntity::STMT);
+        std::unordered_set<std::string> expectedResults = { "4", "8" };
+        REQUIRE(testResults == expectedResults);
+    }
+
+    SECTION("Parent with synonym and wildcard") {
+        // Parent(_, a)
+        std::unordered_set<std::string> testResults = pkbSP->getRelationshipWithFirstWildcard(RelationshipType::PARENT, DesignEntity::ASSIGN);
+        std::unordered_set<std::string> expectedResults = { "5", "6", "7", "9", "10", "11" };
+        REQUIRE(testResults == expectedResults);
+    }
+
     SECTION("Parent with 2 synonyms") {
         // Parent(if, s)
         std::unordered_map<std::string, std::unordered_set<std::string>> testResults = pkbSP->getAllRelationship(RelationshipType::PARENT, DesignEntity::IF, DesignEntity::STMT);
         std::unordered_map<std::string, std::unordered_set<std::string>> expectedResults = { {"8", std::unordered_set<std::string>({"9", "10", "11"})} };
         REQUIRE(testResults == expectedResults);
     }
+}
 
+TEST_CASE("Parent* Relationship for SP") {
     SECTION("Parent* with no synonym") {
         // Parent*(4, 5)
         bool testResults = pkbSP->getRelationship(RelationshipType::PARENT_T, TokenObject(TokenType::INTEGER, "4"), TokenObject(TokenType::INTEGER, "5"));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Parent* with value and wildcard") {
+        // Parent*(8, _)
+        bool testResults = pkbSP->getRelationship(RelationshipType::PARENT_T, TokenObject(TokenType::INTEGER, "8"), TokenObject(TokenType::WILDCARD, ""));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Parent* with wildcard and value") {
+        // Parent*(_, 9)
+        bool testResults = pkbSP->getRelationship(RelationshipType::PARENT_T, TokenObject(TokenType::WILDCARD, ""), TokenObject(TokenType::INTEGER, "9"));
+        REQUIRE(testResults);
+    }
+
+    SECTION("Parent* with wildcard and wildcard") {
+        // Parent*(_, _)
+        bool testResults = pkbSP->getRelationship(RelationshipType::PARENT_T, TokenObject(TokenType::WILDCARD, ""), TokenObject(TokenType::WILDCARD, ""));
         REQUIRE(testResults);
     }
 
@@ -281,14 +432,29 @@ TEST_CASE("Relationships and patterns for SP") {
         REQUIRE(testResults == expectedResults);
     }
 
+    SECTION("Parent* with wildcard and synonym") {
+        // Parent*(if, _)
+        std::unordered_set<std::string> testResults = pkbSP->getRelationshipWithSecondWildcard(RelationshipType::PARENT, DesignEntity::IF);
+        std::unordered_set<std::string> expectedResults = { "8" };
+        REQUIRE(testResults == expectedResults);
+    }
+
+    SECTION("Parent* with synonym and wildcard") {
+        // Parent*(_, s)
+        std::unordered_set<std::string> testResults = pkbSP->getRelationshipWithFirstWildcard(RelationshipType::PARENT, DesignEntity::STMT);
+        std::unordered_set<std::string> expectedResults = { "5", "6", "7", "9", "10", "11" };
+        REQUIRE(testResults == expectedResults);
+    }
+
     SECTION("Parent* with 2 synonyms") {
         // Parent*(w, s)
         std::unordered_map<std::string, std::unordered_set<std::string>> testResults = pkbSP->getAllRelationship(RelationshipType::PARENT_T, DesignEntity::WHILE, DesignEntity::STMT);
         std::unordered_map<std::string, std::unordered_set<std::string>> expectedResults = { {"4", std::unordered_set<std::string>({"5", "6", "7"})} };
         REQUIRE(testResults == expectedResults);
     }
+}
 
-
+TEST_CASE("Assign Pattern for SP") {
     SECTION("Pattern with exact expression and exact expression") {
         // a("count", "0")
         std::unordered_set<std::string> testResults = pkbSP->getPattern(DesignEntity::ASSIGN, TokenObject(TokenType::NAME_WITH_QUOTATION, "count"), TokenObject(TokenType::EXPRESSION, "0"));
