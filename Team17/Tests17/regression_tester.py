c12c21c1c635816018135417652c2a7aebe65e7d
Andrea Tan <andreaxtan611@gmail.com>
2022-10-28 00:20:10 +0800

    Add regresstion tester script

--- Team17/Tests17/regression_tester.py
+++ Team17/Tests17/regression_tester.py
@@ -32,17 +32,74 @@ def get_tests_paths_pairs():
     return test_pairs
 
 
+def get_results():
+    tree = ET.parse("../Code17/tests/out.xml")
+    queries = tree.getroot()[1]
+    queries_count = 0
+    correct_id = []
+    wrong_id = []
+    timeout_id = []
+    for query in queries:
+        queries_count += 1
+        query_id = query[0].text
+        tag_type = query[2].tag
+
+        if tag_type == "timeout":
+            timeout_id.append(query_id)
+            continue
+        if tag_type == "exception":
+            wrong_id.append(query_id)
+            continue
+
+        is_result_tag = query[5].tag
+        if is_result_tag == "failed":
+            wrong_id.append(query_id)
+            continue
+        correct_id.append(query_id)
+
+    return queries_count, correct_id, wrong_id, timeout_id
+
+
 def run_autotester(autotester_path, test_path_pairs):
+    total_count = 0
+    total_correct_count = 0
+    total_wrong_count = 0
+    total_timeout_count = 0
+    run_count = 0
     for source_path, query_path in test_path_pairs:
+        run_count += 1
         subprocess.run([autotester_path, source_path, query_path, "../Code17/tests/out.xml"], stdout=subprocess.DEVNULL)
+        print("---------- AUTOTESTER RUNNING COMPLETED ------------")
+        try:
+            queries_count, correct_id, wrong_id, timeout_id = get_results()
+        except ET.ParseError:
+            raise RuntimeError("Unable to parse out.xml")
+
+        total_count += queries_count
+        total_correct_count += len(correct_id)
+        total_wrong_count += len(wrong_id)
+        total_timeout_count += len(timeout_id)
+
+        print(f"[{query_path}] {len(correct_id)}/{queries_count} passing test cases for run {run_count}.")
+
+        if len(wrong_id) > 0:
+            print(f"\tWrong cases: {wrong_id}")
+        if len(timeout_id) > 0:
+            print(f"\tTimeout cases: {timeout_id}")
+
+    return total_count, total_correct_count, total_wrong_count, total_timeout_count
 
 
 if __name__ == "__main__":
     try:
         test_path_pairs = get_tests_paths_pairs()
-        run_autotester(sys.argv[1], test_path_pairs)
-        print("---------- AUTOTESTER RUNNING COMPLETED ------------")
+        total_count, total_correct_count, total_wrong_count, total_timeout_count = run_autotester(sys.argv[1], test_path_pairs)
+
+        print(f"Correct test cases: {total_correct_count}/{total_count}.")
+        print(f"Wrong test cases: {total_wrong_count + total_timeout_count}/{total_count}.")
     except Exception as e:
         print(e)
-        sys.exit(1)
+
+
+
 
