806f9979d950def62fb01fd43db4367bcaebd48c
Kacalamaka <heinrich2001.ang@gmail.com>
2022-10-28 02:02:14 +0800

    Added method to handle Wildcard Query

--- Team17/Code17/src/spa/src/components/pkb/runtimeEvaluator/NextTRelationshipEvaluator.cpp
+++ Team17/Code17/src/spa/src/components/pkb/runtimeEvaluator/NextTRelationshipEvaluator.cpp
@@ -70,10 +70,44 @@ void NextTRelationshipEvaluator::DFSNextTWithTwoSynonyms(std::unordered_set<std:
 	}
 }
 
+void NextTRelationshipEvaluator::DFSNextTWildcardForward(std::unordered_set<std::string>& filter1,
+	std::unordered_set<std::string>& filter2, std::unordered_set<std::string>& result) {
+	for (const auto& ele : filter1) {
+		std::unordered_set<std::string> visited;
+		std::unordered_set<std::string> result;
+		std::string start = ele;
+		DFSNextTForwardWithSynonym(start, visited, result, filter2);
+		if (result.size() != 0) {
+			result.insert(start);
+		}
+	}
+}
+
+void NextTRelationshipEvaluator::DFSNextTWildcardBackward(std::unordered_set<std::string>& filter1,
+	std::unordered_set<std::string>& filter2, std::unordered_set<std::string>& result) {
+	for (const auto& ele : filter2) {
+		std::unordered_set<std::string> visited;
+		std::unordered_set<std::string> result;
+		std::string start = ele;
+		DFSNextTBackwardWithSynonym(start, visited, result, filter1);
+		if (result.size() != 0) {
+			result.insert(start);
+		}
+	}
+}
+
 bool NextTRelationshipEvaluator::getRuntimeRelationship(RelationshipType relType, TokenObject firstArgument, TokenObject secondArgument) {
 	if (relType == RelationshipType::NEXT_T) {
-		std::unordered_set<std::string> visited;
-		return DFSNextTForward(firstArgument.getValue(), secondArgument.getValue(), visited);
+		if (firstArgument.getTokenType() == TokenType::INTEGER && secondArgument.getTokenType() == TokenType::INTEGER) {
+			return handleConstantConstant(firstArgument, secondArgument);
+		} else if (firstArgument.getTokenType() == TokenType::WILDCARD && secondArgument.getTokenType() == TokenType::INTEGER) {
+			return handleWildcardConstant(secondArgument);
+		} else if (firstArgument.getTokenType() == TokenType::CONSTANT && secondArgument.getTokenType() == TokenType::WILDCARD) {
+			return handleConstantWildcard(firstArgument);
+		}
+		else if (firstArgument.getTokenType() == TokenType::WILDCARD && secondArgument.getTokenType() == TokenType::WILDCARD) {
+			return handleWildcardWildcard();
+		}
 	}
 
 	return false;
@@ -108,4 +142,40 @@ std::unordered_map<std::string, std::unordered_set<std::string>> NextTRelationsh
 		return result_map;
 	}
 	return std::unordered_map<std::string, std::unordered_set<std::string>>();
+}
+
+std::unordered_set<std::string> NextTRelationshipEvaluator::getRuntimeRelationshipWithFirstWildcard(RelationshipType relType, std::unordered_set<std::string>& filter1, std::unordered_set<std::string>& filter2) {
+	if (relType == RelationshipType::NEXT_T) {
+		std::unordered_set<std::string> result;
+		DFSNextTWildcardForward(filter1, filter2, result);
+		return result;
+	}
+	return std::unordered_set<std::string>();
+}
+std::unordered_set<std::string> NextTRelationshipEvaluator::getRuntimeRelationshipWithSecondWildcard(RelationshipType relType, std::unordered_set<std::string>& filter1, std::unordered_set<std::string>& filter2) {
+	if (relType == RelationshipType::NEXT_T) {
+		std::unordered_set<std::string> result;
+		DFSNextTWildcardBackward(filter1, filter2, result);
+		return result;
+	}
+	return std::unordered_set<std::string>();
+}
+
+bool NextTRelationshipEvaluator::handleConstantConstant(TokenObject firstArgument, TokenObject secondArgument) {
+	std::unordered_set<std::string> visited;
+	return DFSNextTForward(firstArgument.getValue(), secondArgument.getValue(), visited);
+}
+
+bool NextTRelationshipEvaluator::handleConstantWildcard(TokenObject firstArgument) {
+	std::unordered_set<std::string> nextStore = nextStorage->getNextForward(firstArgument.getValue());
+	return nextStore.size() != 0;
+}
+
+bool NextTRelationshipEvaluator::handleWildcardConstant(TokenObject secondArgument) {
+	std::unordered_set<std::string> nextStore = nextStorage->getNextBackward(secondArgument.getValue());
+	return nextStore.size() != 0;
+}
+
+bool NextTRelationshipEvaluator::handleWildcardWildcard() {
+	return !nextStorage->isEmpty();
 }
--- Team17/Code17/src/spa/src/components/pkb/runtimeEvaluator/NextTRelationshipEvaluator.h
+++ Team17/Code17/src/spa/src/components/pkb/runtimeEvaluator/NextTRelationshipEvaluator.h
@@ -16,6 +16,8 @@ public:
 	bool getRuntimeRelationship(RelationshipType relType, TokenObject firstArgument, TokenObject secondArgument);
 	std::unordered_set<std::string> getRuntimeRelationshipByFirst(RelationshipType relType, TokenObject firstArgument, std::unordered_set<std::string>& filter);
 	std::unordered_set<std::string> getRuntimeRelationshipBySecond(RelationshipType relType, TokenObject secondArgument, std::unordered_set<std::string>& filter);
+	std::unordered_set<std::string> getRuntimeRelationshipWithFirstWildcard(RelationshipType relType, std::unordered_set<std::string>& filter1, std::unordered_set<std::string>& filter2);
+	std::unordered_set<std::string> getRuntimeRelationshipWithSecondWildcard(RelationshipType relType, std::unordered_set<std::string>& filter1, std::unordered_set<std::string>& filter2);
 	std::unordered_map<std::string, std::unordered_set<std::string>> getAllRuntimeRelationship(RelationshipType relType, std::unordered_set<std::string>& filter1, std::unordered_set<std::string>& filter2);
 private:
 	bool DFSNextTForward(std::string curr, std::string target, std::unordered_set<std::string>& visited);
@@ -26,6 +28,17 @@ private:
 	void DFSNextTWithTwoSynonyms(std::unordered_set<std::string>& filter1, 
 						std::unordered_set<std::string>& filter2, 
 						std::unordered_map<std::string, std::unordered_set<std::string>>& result_map);
+	void DFSNextTWildcardForward(std::unordered_set<std::string>& filter1,
+						std::unordered_set<std::string>& filter2,
+						std::unordered_set<std::string>& result);
+	void DFSNextTWildcardBackward(std::unordered_set<std::string>& filter1,
+		std::unordered_set<std::string>& filter2,
+		std::unordered_set<std::string>& result);
+	bool handleConstantConstant(TokenObject firstArgument, TokenObject secondArgument);
+	bool handleConstantWildcard(TokenObject firstArgument);
+	bool handleWildcardConstant(TokenObject secondArgument);
+	bool handleWildcardWildcard();
+
 
 	NextRelationshipStorage* nextStorage;
 };
